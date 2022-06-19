51fac438cf27ae0f9f7e2a1a4067e336e9a2fdb6
Glory-Day <alwaysgloryday@gmail.com>
2022-06-20 01:24:44 +0900

    Upload Script VideoUtility.cs

---
+++ Connexion (Girl's Frontline)/Assets/Scripts/Main/VideoUtility.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+namespace Main
+{
+    public class VideoUtility : MonoBehaviour
+    {
+        private VideoPlayer videoPlayer;
+        
+        // Start is called before the first frame update
+        private void Start()
+        {
+            videoPlayer = GetComponent<VideoPlayer>();
+        }
+
+        // Update is called once per frame
+        private void Update()
+        {
+        
+        }
+    }
+}
