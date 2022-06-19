0c9441cfce7bbabf5ddce32f342dfac6de2abebc
Glory-Day <alwaysgloryday@gmail.com>
2022-06-20 01:23:27 +0900

    Upload Script DataManager.cs

---
+++ Connexion (Girl's Frontline)/Assets/Scripts/Manager/Data/DataManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Manager.Data
+{
+    public class DataManager : Singleton<DataManager>
+    {
+        protected DataManager()
+        {
+            // Guarantee this object will be always a singleton only - Can not use the constructor
+        }
+    }
+}
