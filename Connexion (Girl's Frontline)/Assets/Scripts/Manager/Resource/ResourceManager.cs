06a243f9d9be450535fd0afdd2ee7d890c7d3fd0
Glory-Day <alwaysgloryday@gmail.com>
2022-06-20 01:24:23 +0900

    Upload Script ResourceManager.cs

---
+++ Connexion (Girl's Frontline)/Assets/Scripts/Manager/Resource/ResourceManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Manager.Resource
+{
+    public class ResourceManager : Singleton<ResourceManager>
+    {
+        protected ResourceManager()
+        {
+            // Guarantee this object will be always a singleton only - Can not use the constructor
+        }
+    }
+}
