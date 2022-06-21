9d22b1522c08849861ed9b6ca398f35900bf34c1
GloryDay <alwaysgloryday@gmail.com>
2022-06-22 00:45:46 +0900

    Upload Script Label.cs

---
+++ Connexion (Girl's Frontline)/Assets/Scripts/Manager/Log/Console/Label.cs
@@ -0,0 +1,22 @@
+﻿namespace Manager.Log.Console
+{
+    public static class Label
+    {
+        // Log Label Text
+        public const string ErrorLogLabel = "[ERROR]";
+        public const string WarningLogLabel = "[WARNING]";
+        public const string SuccessLogLabel = "[SUCCESS]";
+
+        // Log Label Color
+        public const string DefaultLogColor = "#C0C0C0";
+        public const string ErrorLogColor = "#DC143C";
+        public const string WarningLogColor = "#F7E600";
+        public const string SuccessLogColor = "#39FF14";
+        
+        // Log Label Type
+        public enum LabelType
+        {
+            Error = 0, Warning, Success
+        }
+    }
+}
