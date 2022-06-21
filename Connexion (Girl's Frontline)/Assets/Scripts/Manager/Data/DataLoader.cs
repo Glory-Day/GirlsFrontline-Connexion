f49ceb070340e9ddada5559b15f7dc38d77b2140
GloryDay <alwaysgloryday@gmail.com>
2022-06-22 00:45:06 +0900

    Upload Script DataLoader.cs

---
+++ Connexion (Girl's Frontline)/Assets/Scripts/Manager/Data/DataLoader.cs
@@ -0,0 +1,50 @@
+﻿using System.IO;
+using UnityEngine;
+
+using Manager.Log;
+using LabelType = Manager.Log.Console.Label.LabelType;
+
+namespace Manager.Data
+{
+    public static class DataLoader<T> where T : class
+    {
+        /// <summary>
+        /// Read the Json file from the corresponding file path and save it in class format
+        /// </summary>
+        /// <param name="path"> Json file path </param>
+        /// <returns> Json file stored in class format </returns>
+        public static T OnLoadData(string path)
+        {
+            T data;
+
+            try
+            {
+#if UNITY_EDITOR
+
+                LogManager.OnDebugLog(
+                    typeof(DataLoader<T>), 
+                    $"Called OnLoadData<{typeof(T).Name}>()");
+
+#endif
+                
+                data = JsonUtility.FromJson<T>(File.ReadAllText(Application.dataPath + path));
+            }
+            catch (DirectoryNotFoundException error)
+            {
+#if UNITY_EDITOR
+
+                LogManager.OnDebugLog(
+                    LabelType.Error,
+                    typeof(DataLoader<T>),
+                    error.Message);
+
+#endif
+
+                // Directory not found exception error
+                return null;
+            }
+            
+            return data;
+        }
+    }
+}
