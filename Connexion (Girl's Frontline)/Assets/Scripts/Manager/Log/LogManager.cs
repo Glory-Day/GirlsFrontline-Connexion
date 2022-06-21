925ca6a05dd8cded435c0a967d9c0448f2b6c789
GloryDay <alwaysgloryday@gmail.com>
2022-06-22 00:44:46 +0900

    Update Script LogManager.cs

--- Connexion (Girl's Frontline)/Assets/Scripts/Manager/Log/LogManager.cs
+++ Connexion (Girl's Frontline)/Assets/Scripts/Manager/Log/LogManager.cs
@@ -1,6 +1,7 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
 using UnityEngine;
+
+using Manager.Log.Console;
 
 namespace Manager.Log
 {
@@ -10,5 +11,26 @@ namespace Manager.Log
         {
             // Guarantee this object will be always a singleton only - Can not use the constructor
         }
+
+        #region CONSOLE STATIC API
+
+        /// <summary>
+        /// General log information is output to the console
+        /// </summary>
+        /// <param name="classType"> The type of the class where the log was called </param>
+        /// <param name="contexts"> Contents of the log </param>
+        public static void OnDebugLog(Type classType, string contexts) => 
+            Debug.Log(LogBuilder.OnBuild(classType, contexts));
+        
+        /// <summary>
+        /// Specific log information is output to the console
+        /// </summary>
+        /// <param name="type"> Type of Log </param>
+        /// <param name="classType"> The type of the class where the log was called </param>
+        /// <param name="contexts"> Contents of the log </param>
+        public static void OnDebugLog(Label.LabelType type, Type classType, string contexts) => 
+            Debug.Log(LogBuilder.OnBuild(type, classType, contexts));
+
+        #endregion
     }
 }
