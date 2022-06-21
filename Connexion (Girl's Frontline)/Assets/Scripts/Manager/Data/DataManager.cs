8d567faacc35d92c799a1262a1d0736a45f71683
GloryDay <alwaysgloryday@gmail.com>
2022-06-22 00:45:37 +0900

    Update Script DataManager.cs

--- Connexion (Girl's Frontline)/Assets/Scripts/Manager/Data/DataManager.cs
+++ Connexion (Girl's Frontline)/Assets/Scripts/Manager/Data/DataManager.cs
@@ -1,14 +1,36 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using Manager.Log;
 
 namespace Manager.Data
 {
     public class DataManager : Singleton<DataManager>
     {
+        
+
         protected DataManager()
         {
             // Guarantee this object will be always a singleton only - Can not use the constructor
         }
+        
+        public static void OnInitializeAudioLabelData()
+        {
+#if UNITY_EDITOR
+
+            LogManager.OnDebugLog(
+                typeof(DataManager), 
+                $"Called OnInitializeAudioData()");
+
+#endif
+            
+            Audio = DataLoader<Category.Label>.OnLoadData(JsonAddresses.Audio);
+            
+            SoundManager.OnInitializeAudioData();
+        }
+
+        #region STATIC API
+
+        public static Category.Label Audio { get; private set; }
+
+        #endregion
+
     }
 }
