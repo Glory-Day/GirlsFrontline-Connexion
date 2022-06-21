c226f7c3b16e50d44e0d531cfc722a67b4cbc6ef
GloryDay <alwaysgloryday@gmail.com>
2022-06-22 00:48:56 +0900

    Update Script ResourceManager.cs

--- Connexion (Girl's Frontline)/Assets/Scripts/Manager/Resource/ResourceManager.cs
+++ Connexion (Girl's Frontline)/Assets/Scripts/Manager/Resource/ResourceManager.cs
@@ -1,6 +1,5 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using Manager.Data;
+using Manager.Log;
 
 namespace Manager.Resource
 {
@@ -10,5 +9,44 @@ namespace Manager.Resource
         {
             // Guarantee this object will be always a singleton only - Can not use the constructor
         }
+
+        public static void OnInitializeAudioAssets()
+        {
+#if UNITY_EDITOR
+
+            LogManager.OnDebugLog(
+                typeof(ResourceManager), 
+                $"Called OnInitializeAudioAssets()");
+
+#endif
+            
+            DataManager.OnInitializeAudioLabelData();
+            
+            InitializeAudioAssets();
+        }
+
+        private static void InitializeAudioAssets()
+        {
+#if UNITY_EDITOR
+
+            LogManager.OnDebugLog(
+                typeof(ResourceManager), 
+                $"Called InitializeAudioAssets()");
+
+#endif
+            
+            AudioAssetLoader.OnLoadBackgroundAudioResources();
+            
+            //DEBUG: This code is not working yet
+            // Resources with labels "effect" and "voice" are available after registration
+            // AudioAssetLoader.OnLoadEffectAudioResources();
+            // AudioAssetLoader.OnLoadVoiceAudioResources();
+        }
+
+        #region STATIC API
+        
+        
+
+        #endregion
     }
 }
