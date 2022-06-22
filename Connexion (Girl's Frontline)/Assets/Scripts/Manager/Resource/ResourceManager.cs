960dcb82427a99aeddb8d71fae452d5defac4d0e
GloryDay <alwaysgloryday@gmail.com>
2022-06-22 16:14:12 +0900

    Update Script ResourceManager.cs

--- Connexion (Girl's Frontline)/Assets/Scripts/Manager/Resource/ResourceManager.cs
+++ Connexion (Girl's Frontline)/Assets/Scripts/Manager/Resource/ResourceManager.cs
@@ -3,6 +3,9 @@ using Manager.Log;
 
 namespace Manager.Resource
 {
+    /// <summary>
+    /// Class to manage resource needed for the game
+    /// </summary>
     public class ResourceManager : Singleton<ResourceManager>
     {
         protected ResourceManager()
@@ -10,6 +13,32 @@ namespace Manager.Resource
             // Guarantee this object will be always a singleton only - Can not use the constructor
         }
 
+        /// <summary>
+        /// Initialize audio assets
+        /// </summary>
+        private static void InitializeAudioAssets()
+        {
+#if UNITY_EDITOR
+
+            LogManager.OnDebugLog(
+                typeof(ResourceManager), 
+                $"Called InitializeAudioAssets()");
+
+#endif
+            
+            AudioAssetLoader.OnLoadBackgroundAudioAssets();
+            
+            //DEBUG: This code is not working yet
+            // Resources with labels "effect" and "voice" are available after registration
+            // AudioAssetLoader.OnLoadEffectAudioAssets();
+            // AudioAssetLoader.OnLoadVoiceAudioAssets();
+        }
+
+        #region STATIC API
+        
+        /// <summary>
+        /// After initializing the audio related label, the audio assets are initialized
+        /// </summary>
         public static void OnInitializeAudioAssets()
         {
 #if UNITY_EDITOR
@@ -24,28 +53,6 @@ namespace Manager.Resource
             
             InitializeAudioAssets();
         }
-
-        private static void InitializeAudioAssets()
-        {
-#if UNITY_EDITOR
-
-            LogManager.OnDebugLog(
-                typeof(ResourceManager), 
-                $"Called InitializeAudioAssets()");
-
-#endif
-            
-            AudioAssetLoader.OnLoadBackgroundAudioResources();
-            
-            //DEBUG: This code is not working yet
-            // Resources with labels "effect" and "voice" are available after registration
-            // AudioAssetLoader.OnLoadEffectAudioResources();
-            // AudioAssetLoader.OnLoadVoiceAudioResources();
-        }
-
-        #region STATIC API
-        
-        
 
         #endregion
     }
