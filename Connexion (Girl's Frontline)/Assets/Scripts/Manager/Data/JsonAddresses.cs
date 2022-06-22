77ca4d72ede2ed086e695c68d2f96fffe151ce19
GloryDay <alwaysgloryday@gmail.com>
2022-06-22 16:13:21 +0900

    Update Script JsonAddresses.cs

--- Connexion (Girl's Frontline)/Assets/Scripts/Manager/Data/JsonAddresses.cs
+++ Connexion (Girl's Frontline)/Assets/Scripts/Manager/Data/JsonAddresses.cs
@@ -1,5 +1,8 @@
 ﻿namespace Manager.Data
 {
+    /// <summary>
+    /// Class with addresses in the Json file
+    /// </summary>
     public static class JsonAddresses
     {
         public const string Audio = "/Data/Audio.json";
