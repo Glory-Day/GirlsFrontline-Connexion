de70e3181210a2fcfc05d71d4f380decc675ead4
GloryDay <alwaysgloryday@gmail.com>
2022-06-22 16:14:23 +0900

    Update Script SoundManager.cs

--- Connexion (Girl's Frontline)/Assets/Scripts/Manager/SoundManager.cs
+++ Connexion (Girl's Frontline)/Assets/Scripts/Manager/SoundManager.cs
@@ -6,6 +6,9 @@ using Manager.Log;
 
 namespace Manager
 {
+    /// <summary>
+    /// Class to manage sound needed for the game
+    /// </summary>
     public class SoundManager : Singleton<SoundManager>
     {
         private static Dictionary<string, AudioClip> _backgroundAudioClips;
@@ -16,15 +19,17 @@ namespace Manager
         {
             // Guarantee this object will be always a singleton only - Can not use the constructor
         }
-
-        // Start is called before the first frame update
-        public static void OnInitializeAudioData()
+        
+        /// <summary>
+        /// Initialize audios
+        /// </summary>
+        public static void OnInitializeAudios()
         {
 #if UNITY_EDITOR
 
             LogManager.OnDebugLog(
                 typeof(SoundManager), 
-                $"Called OnInitializeAudioData()");
+                $"Called OnInitializeAudios()");
 
 #endif
             
@@ -35,17 +40,49 @@ namespace Manager
 
         #region STATIC API
 
+        /// <summary>
+        /// Add background audio with name
+        /// </summary>
+        /// <param name="key"> Name of background audio </param>
+        /// <param name="audioClip"> Background audio </param>
         public static void AddBackgroundAudioClip(string key, AudioClip audioClip) => 
             _backgroundAudioClips.Add(key, audioClip);
         
+        /// <summary>
+        /// Add effect audio with name
+        /// </summary>
+        /// <param name="key"> Name of effect audio </param>
+        /// <param name="audioClip"> Effect audio </param>
         public static void AddEffectAudioClip(string key, AudioClip audioClip) => 
             _effectAudioClips.Add(key, audioClip);
         
+        /// <summary>
+        /// Add voice audio with name
+        /// </summary>
+        /// <param name="key"> Name of voice audio </param>
+        /// <param name="audioClip"> Voice audio </param>
         public static void AddVoiceAudioClip(string key, AudioClip audioClip) => 
             _voiceAudioClips.Add(key, audioClip);
 
+        /// <summary>
+        /// Search for and return background audio by name
+        /// </summary>
+        /// <param name="key"> Background audio name to search for </param>
+        /// <returns> Discovered background audio </returns>
         public static AudioClip GetBackgroundAudioClip(string key) => _backgroundAudioClips[key];
+        
+        /// <summary>
+        /// Search for and return effect audio by name
+        /// </summary>
+        /// <param name="key"> Effect audio name to search for </param>
+        /// <returns> Discovered effect audio </returns>
         public static AudioClip GetEffectAudioClip(string key) => _effectAudioClips[key];
+        
+        /// <summary>
+        /// Search for and return voice audio by name
+        /// </summary>
+        /// <param name="key"> Voice audio name to search for </param>
+        /// <returns> Discovered voice audio </returns>
         public static AudioClip GetVoiceAudioClip(string key) => _voiceAudioClips[key];
 
         #endregion
