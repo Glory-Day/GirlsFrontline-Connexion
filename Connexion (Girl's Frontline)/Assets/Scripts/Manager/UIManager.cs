88b7d9efaf97a6b8eb75c6454cbc0b8ff27a64c3
GloryDay <alwaysgloryday@gmail.com>
2022-06-24 00:13:23 +0900

    Upload Script UIManager.cs

---
+++ Connexion (Girl's Frontline)/Assets/Scripts/Manager/UIManager.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Manager
+{
+    public class UIManager : Singleton<UIManager>
+    {
+        private Canvas sceneTransitionCanvas;
+        
+        protected UIManager()
+        {
+            // Guarantee this object will be always a singleton only - Can not use the constructor
+        }
+    }
+}
