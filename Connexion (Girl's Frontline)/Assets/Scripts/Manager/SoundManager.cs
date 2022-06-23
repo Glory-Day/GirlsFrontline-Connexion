9e3f88a9a540d1c4b712c89b94bea9e94e0dfb74
GloryDay <alwaysgloryday@gmail.com>
2022-06-24 00:15:02 +0900

    Update Script SoundManager.cs

--- Connexion (Girl's Frontline)/Assets/Scripts/Manager/SoundManager.cs
+++ Connexion (Girl's Frontline)/Assets/Scripts/Manager/SoundManager.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 using Manager.Log;
+using Manager.Log.Console;
 
 namespace Manager
 {
@@ -12,13 +13,30 @@ namespace Manager
     /// </summary>
     public class SoundManager : Singleton<SoundManager>
     {
-        private static Dictionary<string, AudioClip> _backgroundAudioClips;
-        private static Dictionary<string, AudioClip> _effectAudioClips;
-        private static Dictionary<string, AudioClip> _voiceAudioClips;
+        [Header("# Playing Background Audio Clip")]
+        [SerializeField] 
+        public AudioClip playingBackgroundAudioClip;
         
+        /// <summary>
+        /// Audio source playing background audio in <b>SoundManager</b> class
+        /// </summary>
+        private AudioSource backgroundAudioSource;
+        
+        private List<AudioClip> backgroundAudioClips;
+        private List<AudioClip> effectAudioClips;
+        private List<AudioClip> voiceAudioClips;
+
+        private const string None = "None";
+
         protected SoundManager()
         {
             // Guarantee this object will be always a singleton only - Can not use the constructor
+        }
+
+        private void Awake()
+        {
+            backgroundAudioSource = gameObject.AddComponent<AudioSource>();
+            backgroundAudioSource.loop = true;
         }
 
         /// <summary>
@@ -29,57 +47,94 @@ namespace Manager
             LogManager.OnDebugLog(typeof(SoundManager), 
                 $"Called OnInitializeAudioClips()");
 
-            _backgroundAudioClips = new Dictionary<string, AudioClip>();
-            _effectAudioClips = new Dictionary<string, AudioClip>();
-            _voiceAudioClips = new Dictionary<string, AudioClip>();
+            Instance.backgroundAudioClips = new List<AudioClip>();
+            Instance.effectAudioClips     = new List<AudioClip>();
+            Instance.voiceAudioClips      = new List<AudioClip>();
+        }
+
+        /// <summary>
+        /// Play background audio select by index
+        /// </summary>
+        /// <param name="index"> Index of background audio clips </param>
+        private static void PlayBackgroundAudio(int index)
+        {
+            LogManager.OnDebugLog(typeof(SoundManager), 
+                $"Called PlayBackgroundAudio()");
+
+            var audioClipName = Instance.playingBackgroundAudioClip != null
+                ? Instance.playingBackgroundAudioClip.name
+                : None;
+
+            var audioClip = Instance.backgroundAudioClips[index];
+
+            if (audioClipName.Equals(audioClip.name)) return;
+
+            LogManager.OnDebugLog(Label.LabelType.Event, typeof(SoundManager), 
+                $"Change background audio clip <b>{audioClipName}</b> to <b>{audioClip.name}</b>");
+            
+            Instance.backgroundAudioSource.clip = Instance.playingBackgroundAudioClip = audioClip;
+            Instance.backgroundAudioSource.Play();
         }
 
         #region STATIC API
+        
+        /// <summary>
+        /// Change the background audio clip when the scene changes
+        /// </summary>
+        /// <param name="name"> Name of the scene that changes </param>
+        /// <exception cref="ArgumentOutOfRangeException"></exception>
+        public static void OnChangeBackgroundAudioClip(SceneManager.SceneName name)
+        {
+            LogManager.OnDebugLog(typeof(SoundManager), 
+                $"Called OnChangeBackgroundAudioClip()");
+            
+            switch (name)
+            {
+                case SceneManager.SceneName.MainScene:
+                    PlayBackgroundAudio(0);
+                    break;
+                case SceneManager.SceneName.SelectionScene:
+                    PlayBackgroundAudio(0);
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(name), name, null);
+            }
+        }
 
         /// <summary>
         /// Add background audio clip with name
         /// </summary>
-        /// <param name="key"> Name of background audio clip </param>
         /// <param name="audioClip"> Background audio clip </param>
-        public static void AddBackgroundAudioClip(string key, AudioClip audioClip) => 
-            _backgroundAudioClips.Add(key, audioClip);
+        public static void AddBackgroundAudioClip(AudioClip audioClip) => 
+            Instance.backgroundAudioClips.Add(audioClip);
         
         /// <summary>
         /// Add effect audio clip with name
         /// </summary>
-        /// <param name="key"> Name of effect audio clip </param>
         /// <param name="audioClip"> Effect audio clip </param>
-        public static void AddEffectAudioClip(string key, AudioClip audioClip) => 
-            _effectAudioClips.Add(key, audioClip);
+        public static void AddEffectAudioClip(AudioClip audioClip) => 
+            Instance.effectAudioClips.Add(audioClip);
         
         /// <summary>
         /// Add voice audio clip with name
         /// </summary>
-        /// <param name="key"> Name of voice audio clip </param>
         /// <param name="audioClip"> Voice audio clip </param>
-        public static void AddVoiceAudioClip(string key, AudioClip audioClip) => 
-            _voiceAudioClips.Add(key, audioClip);
+        public static void AddVoiceAudioClip(AudioClip audioClip) => 
+            Instance.voiceAudioClips.Add(audioClip);
 
-        /// <summary>
-        /// Returns background audio clip search by name
-        /// </summary>
-        /// <param name="key"> Background audio clip name to search for </param>
-        /// <returns> Background audio clip </returns>
-        public static AudioClip GetBackgroundAudioClip(string key) => _backgroundAudioClips[key];
-        
         /// <summary>
         /// Returns effect audio clip search by name
         /// </summary>
-        /// <param name="key"> Effect audio clip name to search for </param>
+        /// <param name="index"> Index of effect audio clips </param>
         /// <returns> Effect audio clip </returns>
-        public static AudioClip GetEffectAudioClip(string key) => _effectAudioClips[key];
+        public static AudioClip GetEffectAudioClip(int index) => Instance.effectAudioClips[index];
         
         /// <summary>
         /// Returns voice audio clip search by name
         /// </summary>
-        /// <param name="key"> Voice audio clip name to search for </param>
+        /// <param name="index"> Index of voice audio clips </param>
         /// <returns> Voice audio clip </returns>
-        public static AudioClip GetVoiceAudioClip(string key) => _voiceAudioClips[key];
+        public static AudioClip GetVoiceAudioClip(int index) => Instance.voiceAudioClips[index];
 
         #endregion
     }
