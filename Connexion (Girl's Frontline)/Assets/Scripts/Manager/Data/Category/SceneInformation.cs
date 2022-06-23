9f4cffc11d6c0c672e5351cc629db5b8d36f66d7
GloryDay <alwaysgloryday@gmail.com>
2022-06-23 18:04:52 +0900

    Update Script SceneInformation.cs

---
+++ Connexion (Girl's Frontline)/Assets/Scripts/Manager/Data/Category/SceneInformation.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Manager.Data.Category
+{
+    /// <summary>
+    /// Scene information format in <b>SceneInformation</b> Json file
+    /// </summary>
+    [Serializable]
+    public class SceneInformation
+    {
+        public string[] names;
+    }
+}
