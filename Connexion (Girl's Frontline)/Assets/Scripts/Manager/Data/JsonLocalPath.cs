cf3aaf76cb804de5619b556a344d985f29886095
GloryDay <alwaysgloryday@gmail.com>
2022-06-23 18:05:50 +0900

    Update Script JsonLocalPath.cs

---
+++ Connexion (Girl's Frontline)/Assets/Scripts/Manager/Data/JsonLocalPath.cs
@@ -0,0 +1,11 @@
+﻿namespace Manager.Data
+{
+    /// <summary>
+    /// Local paths in the Json file
+    /// </summary>
+    public static class JsonLocalPath
+    {
+        public const string AddressableLabel = "/Data/AddressableLabel.json";
+        public const string SceneName = "/Data/SceneInformation.json";
+    }
+}
