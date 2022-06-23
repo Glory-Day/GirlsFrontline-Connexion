b4fedcb99dd2391c1132f1187bfd672bfe090281
GloryDay <alwaysgloryday@gmail.com>
2022-06-23 18:06:13 +0900

    Update Script Label.cs

--- Connexion (Girl's Frontline)/Assets/Scripts/Manager/Log/Console/Label.cs
+++ Connexion (Girl's Frontline)/Assets/Scripts/Manager/Log/Console/Label.cs
@@ -1,17 +1,19 @@
 ﻿namespace Manager.Log.Console
 {
     /// <summary>
-    /// Class with label or label-related data in the output log
+    /// Label or label-related data in the output log
     /// </summary>
     public static class Label
     {
         // Log Label Text
+        public const string EventLogLabel = "[EVENT]";
         public const string ErrorLogLabel = "[ERROR]";
         public const string WarningLogLabel = "[WARNING]";
         public const string SuccessLogLabel = "[SUCCESS]";
 
         // Log Label Color
         public const string DefaultLogColor = "#C0C0C0";
+        public const string EventLogColor = "#F8F8FF";
         public const string ErrorLogColor = "#DC143C";
         public const string WarningLogColor = "#F7E600";
         public const string SuccessLogColor = "#39FF14";
@@ -19,7 +21,7 @@
         // Log Label Type
         public enum LabelType
         {
-            Error = 0, Warning, Success
+            Event = 0, Error, Warning, Success
         }
     }
 }
