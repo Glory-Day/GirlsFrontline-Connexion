5bfae1ac61b0a1effcf2fcaae11bd4071ccd12a1
GloryDay <alwaysgloryday@gmail.com>
2022-06-23 18:06:25 +0900

    Update Script LogBuilder.cs

--- Connexion (Girl's Frontline)/Assets/Scripts/Manager/Log/Console/LogBuilder.cs
+++ Connexion (Girl's Frontline)/Assets/Scripts/Manager/Log/Console/LogBuilder.cs
@@ -3,12 +3,12 @@
 namespace Manager.Log.Console
 {
     /// <summary>
-    /// Class building output log
+    /// Output log string builder by purpose
     /// </summary>
     public static class LogBuilder
     {
         /// <summary>
-        /// Builds the string of the default log
+        /// Build the string of the default log
         /// </summary>
         /// <param name="classType"> The type of the class where the log was called </param>
         /// <param name="contents"> Contents of the log </param>
@@ -19,7 +19,7 @@ namespace Manager.Log.Console
         }
         
         /// <summary>
-        /// Builds the string of special log
+        /// Build the string of special log
         /// </summary>
         /// <param name="type"> Type of Log </param>
         /// <param name="classType"> The type of the class where the log was called </param>
@@ -32,6 +32,10 @@ namespace Manager.Log.Console
 
             switch (type)
             {
+                case Label.LabelType.Event:
+                    log = $"<color={Label.EventLogColor}><b>{Label.EventLogLabel}[{classType.Name}]</b> " +
+                          $"{contents}</color>";
+                    break;
                 case Label.LabelType.Error:
                     log = $"<color={Label.ErrorLogColor}><b>{Label.ErrorLogLabel}[{classType.Name}]</b> " +
                           $"{contents}</color>";
