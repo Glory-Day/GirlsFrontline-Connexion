fcfea39f567ca971a534dc99f9a0feea8c3fcfc0
GloryDay <alwaysgloryday@gmail.com>
2022-06-30 00:25:04 +0900

    Upload Script LogBuilder.cs

---
+++ Connexion (Girl's Frontline)/Assets/Scripts/Manager/Log/Build/LogBuilder.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Manager.Log.Build
+{
+    /// <summary>
+    /// Output log string builder by purpose in <b>Unity Application</b> after build
+    /// </summary>
+    public static class LogBuilder
+    {
+        /// <summary>
+        /// Build the string of the default log
+        /// </summary>
+        /// <param name="classType"> The type of the class where the log was called </param>
+        /// <param name="contents"> Contents of the log </param>
+        /// <returns> The string of the default log </returns>
+        public static string OnBuild(Type classType, string contents)
+        {
+            return $"{Label.DefaultLogLabel}|{classType.Name}|Called <i>{contents}</i>";
+        }
+        
+        /// <summary>
+        /// Build the string of special log
+        /// </summary>
+        /// <param name="type"> Type of Log </param>
+        /// <param name="classType"> The type of the class where the log was called </param>
+        /// <param name="contents"> Contents of the log </param>
+        /// <returns> The string of special log </returns>
+        /// <exception cref="ArgumentOutOfRangeException"></exception>
+        public static string OnBuild(Label.LabelType type, Type classType, string contents)
+        {
+            string log;
+
+            switch (type)
+            {
+                case Label.LabelType.Event:
+                    log = $"{Label.EventLogLabel}|{classType.Name}|{contents}";
+                    break;
+                case Label.LabelType.Error:
+                    log = $"{Label.ErrorLogLabel}|{classType.Name}|{contents}";
+                    break;
+                case Label.LabelType.Warning:
+                    log = $"{Label.WarningLogLabel}|{classType.Name}|{contents}";
+                    break;
+                case Label.LabelType.Success:
+                    log = $"{Label.SuccessLogLabel}|{classType.Name}|{contents}";
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(type), type, null);
+            }
+
+            return log;
+        }
+    }
+}
