3a908f7168660848bbb6acddf27b9384161c3d23
GloryDay <alwaysgloryday@gmail.com>
2022-06-30 23:08:06 +0900

    Upload Script MainButtonUtility.cs

---
+++ Connexion (Girl's Frontline)/Assets/Scripts/Main/MainButtonUtility.cs
@@ -0,0 +1,21 @@
+﻿using Manager;
+using Manager.Log;
+using UnityEngine;
+
+namespace Main
+{
+    public class MainButtonUtility : MonoBehaviour
+    {
+        /// <summary>
+        /// Button event to click <b>Game Start Button</b>
+        /// </summary>
+        public void OnClickedGameStartButton()
+        {
+            LogManager.OnDebugLog(Label.LabelType.Event, typeof(MainButtonUtility), 
+                "<b>Game Start Button</b> is clicked");
+            
+            UIManager.SetLeftScreenTransitionAnimation();
+            UIManager.OnPlayScreenTransitionAnimation();
+        }
+    }
+}
