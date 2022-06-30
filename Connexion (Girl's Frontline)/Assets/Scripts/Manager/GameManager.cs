1b6428894ed507eb1b02678828ea049a6600d2d0
GloryDay <alwaysgloryday@gmail.com>
2022-06-30 23:08:32 +0900

    Upload Script GameManager.cs

---
+++ Connexion (Girl's Frontline)/Assets/Scripts/Manager/GameManager.cs
@@ -0,0 +1,13 @@
+﻿using LabelType = Manager.Log.Label.LabelType;
+using SceneName = Manager.SceneManager.SceneName;
+
+namespace Manager
+{
+    public class GameManager : Singleton<GameManager>
+    {
+        protected GameManager()
+        {
+            // Guarantee this object will be always a singleton only - Can not use the constructor
+        }
+    }
+}
