db1b8bf236fb63a9d0609dc373c54bcf096b7e64
GloryDay <alwaysgloryday@gmail.com>
2022-07-03 23:09:54 +0900

    Upload Script ResourceInformation.cs

---
+++ Connexion (Girl's Frontline)/Assets/Scripts/Manager/Data/Category/ResourceInformation.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Manager.Data.Category
+{
+    #region Audio API
+
+    [Serializable]
+    public class AudioMixer
+    {
+        public string[] names;
+    }
+
+    [Serializable]
+    public class BackgroundAudioClip
+    {
+        public string[] names;
+    }
+    
+    [Serializable]
+    public class EffectAudioClip
+    {
+        public string[] names;
+    }
+    
+    [Serializable]
+    public class VoiceAudioClip
+    {
+        public string[] names;
+    }
+
+    #endregion
+
+    #region PREFAB API
+
+    [Serializable]
+    public class UIPrefab
+    {
+        public string[] names;
+    }
+
+    #endregion
+
+    [Serializable]
+    public class ResourceInformation
+    {
+        public AudioMixer audioMixer;
+        public BackgroundAudioClip backgroundAudioClip;
+        public EffectAudioClip effectAudioClip;
+        public VoiceAudioClip voiceAudioClip;
+        public UIPrefab uiPrefab;
+    }
+}
