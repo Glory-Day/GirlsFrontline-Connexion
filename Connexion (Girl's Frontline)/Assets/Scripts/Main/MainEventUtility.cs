39287b6dc15dc75315579ec1852b2bfc78b61f39
GloryDay <alwaysgloryday@gmail.com>
2022-07-17 17:11:38 +0900

    Update Script MainEventUtility.cs

--- Connexion (Girl's Frontline)/Assets/Scripts/Main/MainEventUtility.cs
+++ Connexion (Girl's Frontline)/Assets/Scripts/Main/MainEventUtility.cs
@@ -4,14 +4,17 @@ using UnityEngine;
 
 namespace Main
 {
-    public class MainButtonUtility : MonoBehaviour
+    /// <summary>
+    /// Events in the <b>Main Scene</b>
+    /// </summary>
+    public class MainEventUtility : MonoBehaviour
     {
         /// <summary>
         /// Button event to click <b>Game Start Button</b> in <b>Main Scene</b>
         /// </summary>
         public void OnClickedGameStartButton()
         {
-            LogManager.OnDebugLog(Label.LabelType.Event, typeof(MainButtonUtility), 
+            LogManager.OnDebugLog(Label.LabelType.Event, typeof(MainEventUtility), 
                 "<b>Game Start Button</b> is clicked");
             
             UIManager.SetLeftScreenTransitionAnimation();
