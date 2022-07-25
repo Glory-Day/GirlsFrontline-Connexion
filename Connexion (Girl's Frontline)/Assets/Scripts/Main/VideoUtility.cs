7e628a70fd0d57c4e4a6b21ea220f2ca489286c7
GloryDay <alwaysgloryday@gmail.com>
2022-07-26 00:00:21 +0900

    Update Script VideoUtility.cs

--- Connexion (Girl's Frontline)/Assets/Scripts/Main/VideoUtility.cs
+++ Connexion (Girl's Frontline)/Assets/Scripts/Main/VideoUtility.cs
@@ -54,7 +54,7 @@ namespace Main
             videoPlayer.isLooping = false;
             videoPlayer.loopPointReached += IsOver;
             
-            UIManager.OnInstantiateScreenTransition();
+            UIManager.OnInstantiateAllUIPrefabs();
             
             // Activate the skip button
             skipButton.gameObject.SetActive(true);
