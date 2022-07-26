8c2a6ebdb8c1541159b3b107e6c93898bdf47da2
GloryDay <alwaysgloryday@gmail.com>
2022-07-26 23:48:21 +0900

    Upload Script InitializeBackgroundAudioMixerCommand.cs

---
+++ Connexion (Girl's Frontline)/Assets/Scripts/UI/Console/Command/Sound/InitializeBackgroundAudioMixerCommand.cs
@@ -0,0 +1,23 @@
+﻿#region NAMESPACE API
+
+using Manager;
+using LabelType = Manager.Log.Label.LabelType;
+
+#endregion
+
+namespace UI.Console.Command.Sound
+{
+    public class InitializeBackgroundAudioMixerCommand : ICommand
+    {
+        public void Execute()
+        {
+            LogManager.OnDebugLog(LabelType.Event, typeof(ICommand), 
+                $"Execute as administrator <b><i>OnInitializeBackgroundAudioMixer()</i></b>");
+            
+            SoundManager.OnInitializeBackgroundAudioMixer();
+            
+            LogManager.OnDebugLog(LabelType.Success, typeof(ICommand), 
+                $"<b>Background Audio Mixer</b> is initialized completely");
+        }
+    }
+}
