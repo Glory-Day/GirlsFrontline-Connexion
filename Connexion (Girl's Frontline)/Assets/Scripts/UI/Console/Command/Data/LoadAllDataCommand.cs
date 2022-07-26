5bc982551914ac841a2ce99d5be7794f19c7dc50
GloryDay <alwaysgloryday@gmail.com>
2022-07-26 23:49:47 +0900

    Upload Script LoadAllDataCommand.cs

---
+++ Connexion (Girl's Frontline)/Assets/Scripts/UI/Console/Command/Data/LoadAllDataCommand.cs
@@ -0,0 +1,20 @@
+﻿#region NAMESPACE API
+
+using Manager;
+using LabelType = Manager.Log.Label.LabelType;
+
+#endregion
+
+namespace UI.Console.Command.Data
+{
+    public class LoadAllDataCommand : ICommand
+    {
+        public void Execute()
+        {
+            LogManager.OnDebugLog(LabelType.Event, typeof(ICommand), 
+                $"Execute as administrator <b><i>OnLoadAllData()</i></b>");
+            
+            DataManager.OnLoadAllData();
+        }
+    }
+}
