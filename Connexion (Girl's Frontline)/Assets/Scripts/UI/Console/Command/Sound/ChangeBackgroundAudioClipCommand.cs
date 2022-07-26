720a688863616f6bfd16e8d4ff2ada5328d4a4f2
GloryDay <alwaysgloryday@gmail.com>
2022-07-26 23:48:41 +0900

    Upload Script ChangeBackgroundAudioClipCommand.cs

---
+++ Connexion (Girl's Frontline)/Assets/Scripts/UI/Console/Command/Sound/ChangeBackgroundAudioClipCommand.cs
@@ -0,0 +1,20 @@
+﻿#region NAMESPACE API
+
+using Manager;
+using LabelType = Manager.Log.Label.LabelType;
+
+#endregion
+
+namespace UI.Console.Command.Sound
+{
+    public class ChangeBackgroundAudioClipCommand : ICommand
+    {
+        public void Execute()
+        {
+            LogManager.OnDebugLog(LabelType.Event, typeof(ICommand), 
+                $"Execute as administrator <b><i>OnChangeBackgroundAudioClip()</i></b> for current scene");
+            
+            SoundManager.OnChangeBackgroundAudioClip(SceneManager.CurrentSceneName);
+        }
+    }
+}
