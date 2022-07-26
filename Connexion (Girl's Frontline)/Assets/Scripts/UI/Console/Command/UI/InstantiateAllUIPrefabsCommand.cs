17389bc46eb75c72a07494d3480fbc4e8bb833f7
GloryDay <alwaysgloryday@gmail.com>
2022-07-26 23:48:02 +0900

    Upload Script InstantiateAllUIPrefabsCommand.cs

---
+++ Connexion (Girl's Frontline)/Assets/Scripts/UI/Console/Command/UI/InstantiateAllUIPrefabsCommand.cs
@@ -0,0 +1,20 @@
+﻿#region NAMESPACE API
+
+using Manager;
+using LabelType = Manager.Log.Label.LabelType;
+
+#endregion
+
+namespace UI.Console.Command.UI
+{
+    public class InstantiateAllUIPrefabsCommand : ICommand
+    {
+        public void Execute()
+        {
+            LogManager.OnDebugLog(LabelType.Event, typeof(ICommand), 
+                $"Execute as administrator <b><i>OnInstantiateAllUIPrefabs()</i></b>");
+            
+            UIManager.OnInstantiateAllUIPrefabs();
+        }
+    }
+}
