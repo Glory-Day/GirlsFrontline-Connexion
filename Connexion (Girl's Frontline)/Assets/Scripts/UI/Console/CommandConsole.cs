10aac93994d2ef9a2afc77052150ad7824f287db
GloryDay <alwaysgloryday@gmail.com>
2022-07-26 23:50:28 +0900

    Update Script CommandConsole.cs

--- Connexion (Girl's Frontline)/Assets/Scripts/UI/Console/CommandConsole.cs
+++ Connexion (Girl's Frontline)/Assets/Scripts/UI/Console/CommandConsole.cs
@@ -1,47 +1,131 @@
-﻿using UnityEngine;
+﻿#region NAMESPACE API
+
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+using UnityEngine;
+using UnityEngine.InputSystem;
+using TMPro;
 
 using Manager;
-using UnityEngine.InputSystem;
+using LabelType = Manager.Log.Label.LabelType;
+
+using UI.Console.Command;
+using UI.Console.Command.Data;
+using UI.Console.Command.Resource;
+using UI.Console.Command.Sound;
+using UI.Console.Command.UI;
+using UI.Console.Command.Util;
+
+#endregion
 
 namespace UI.Console
 {
     /// <summary>
-    /// Command console for development game application
+    /// Command console for development <b>Game Application</b>
     /// </summary>
     /// TODO: Command consoles should not be used in the final build because it is used only for development
     public class CommandConsole : MonoBehaviour
     {
-        [Header("# Command Console Components")]
-        [SerializeField] 
-        public GameObject commandInputField;
+        #region SERIALIZABLE FIELD
 
-        private bool isConsoleEnabled;
+        [Header("# Command Console GameObject")]
+        [SerializeField] 
+        public GameObject commandConsoleInputFieldObject;
+
+        [Header("# Command Console Input Field Component")] 
+        [SerializeField]
+        public TMP_InputField commandConsoleInputField;
+
+        #endregion
+
+        private Dictionary<string, ICommand> commands;
+        
+        /// <summary>
+        /// Check <b>Command Console</b> is enabled
+        /// </summary>
+        private bool isCommandConsoleEnabled;
 
         // Awake is called when the script instance is being loaded
         private void Awake()
         {
             transform.SetParent(UIManager.GetTransform());
-            commandInputField.SetActive(false);
-            isConsoleEnabled = false;
+            commandConsoleInputFieldObject.SetActive(false);
+            isCommandConsoleEnabled = false;
         }
 
+        // Start is called before the first frame update
+        private void Start()
+        {
+            commands = new Dictionary<string, ICommand>
+            {
+                { CommandName.LoadAllDataCommand, new LoadAllDataCommand() },
+                { CommandName.LoadAllResourcesCommand, new LoadAllResourceCommand() },
+                { CommandName.UnloadAllResourcesCommand, new UnloadAllResourceCommand() },
+                { CommandName.IsAllResourcesLoadedCommand, new IsLoadedAllResourcesDoneCommand() },
+                { CommandName.InitializeBackgroundAudioMixer, new InitializeBackgroundAudioMixerCommand() },
+                { CommandName.ChangeBackgroundAudioClip, new ChangeBackgroundAudioClipCommand() },
+                { CommandName.InstantiateAllUIPrefabsCommand, new InstantiateAllUIPrefabsCommand() },
+                { CommandName.ApplicationQuit, new ApplicationQuitCommand() }
+            };
+        }
+
+        #region INPUT EVENT API
+
+        /// <summary>
+        /// Event that is turned on and off to use <b>Command Console</b>
+        /// </summary>
         public void OnToggle(InputAction.CallbackContext context)
         {
             if (!context.performed) return;
 
-            switch (isConsoleEnabled)
+            switch (isCommandConsoleEnabled)
             {
                 case false:
+                    LogManager.OnDebugLog(LabelType.Event, typeof(CommandConsole), 
+                        $"Turn on <b>Command Console</b>");
+                    
                     GameManager.OnPause();
-                    commandInputField.SetActive(true);
-                    isConsoleEnabled = true;
+                    commandConsoleInputFieldObject.SetActive(true);
+                    isCommandConsoleEnabled = true;
                     break;
                 case true:
+                    LogManager.OnDebugLog(LabelType.Event, typeof(CommandConsole), 
+                        $"Turn off <b>Command Console</b>");
+                    
                     GameManager.OnPlay();
-                    commandInputField.SetActive(false);
-                    isConsoleEnabled = false;
+                    commandConsoleInputFieldObject.SetActive(false);
+                    isCommandConsoleEnabled = false;
                     break;
             }
         }
+
+        /// <summary>
+        /// Event that input command with <b>Command Console</b>
+        /// </summary>
+        public void OnInput(InputAction.CallbackContext context)
+        {
+            if (!context.performed) return;
+
+            // Get command console input text and clear input field in command console
+            var inputCommand = commandConsoleInputField.text;
+            inputCommand = Regex.Replace(inputCommand, @"\n|\r", "");
+            commandConsoleInputField.text = string.Empty;
+            
+            // Split command if command has pipeline
+            var inputCommands = Regex.Split(inputCommand, @" & ");
+            var lenght = inputCommands.Length;
+
+            // Execute commands sequentially
+            for (var i = 0; i < lenght; i++)
+            {
+                if (!string.IsNullOrEmpty(inputCommands[i]))
+                {
+                    commands[inputCommands[i]].Execute();
+                }
+            }
+        }
+
+        #endregion
     }
 }
