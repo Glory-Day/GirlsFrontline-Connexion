97e1c910e58726ed94018297b86d701b99713d80
GloryDay <alwaysgloryday@gmail.com>
2022-07-26 23:52:58 +0900

    Update Script AddressableLabelData.cs

--- Connexion (Girl's Frontline)/Assets/Scripts/Manager/Data/Category/AddressableLabelData.cs
+++ Connexion (Girl's Frontline)/Assets/Scripts/Manager/Data/Category/AddressableLabelData.cs
@@ -1,9 +1,13 @@
-﻿using System;
+﻿#region NAMESPACE API
+
+using System;
+
+#endregion
 
 namespace Manager.Data.Category
 {
     /// <summary>
-    /// Addressable label data in <b>AddressableLabel</b> Json file
+    /// Addressable label data in <b>AddressableLabel.json</b>
     /// </summary>
     [Serializable]
     public class AddressableLabelData
