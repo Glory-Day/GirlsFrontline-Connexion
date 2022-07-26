c6bee946a16cc7c8839c27a476d3c79f3d1a4e2e
GloryDay <alwaysgloryday@gmail.com>
2022-07-26 23:49:26 +0900

    Upload Script IsLoadedAllResourcesDoneCommand.cs

---
+++ Connexion (Girl's Frontline)/Assets/Scripts/UI/Console/Command/Resource/IsLoadedAllResourcesDoneCommand.cs
@@ -0,0 +1,29 @@
+﻿#region NAMESPACE API
+
+using Manager;
+using LabelType = Manager.Log.Label.LabelType;
+
+#endregion
+
+namespace UI.Console.Command.Resource
+{
+    public class IsLoadedAllResourcesDoneCommand : ICommand
+    {
+        public void Execute()
+        {
+            LogManager.OnDebugLog(LabelType.Event, typeof(ICommand), 
+                $"Execute as administrator <b><i>IsLoadedAllResourcesDone()</i></b>");
+            
+            if (ResourceManager.IsLoadedAllResourcesDone())
+            {
+                LogManager.OnDebugLog(LabelType.Success, typeof(ICommand), 
+                    $"<b>All resources</b> is loaded completely");
+
+                return;
+            }
+            
+            LogManager.OnDebugLog(LabelType.Error, typeof(ICommand), 
+                $"<b>All resources</b> is not loaded");
+        }
+    }
+}
