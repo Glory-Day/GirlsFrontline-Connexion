d72342dfdd446b43c7145309dec0015b39d80286
GloryDay <alwaysgloryday@gmail.com>
2022-07-26 23:48:56 +0900

    Upload Script UnloadAllResourcesCommand.cs

---
+++ Connexion (Girl's Frontline)/Assets/Scripts/UI/Console/Command/Resource/UnloadAllResourcesCommand.cs
@@ -0,0 +1,23 @@
+﻿#region NAMESPACE API
+
+using Manager;
+using LabelType = Manager.Log.Label.LabelType;
+
+#endregion
+
+namespace UI.Console.Command.Resource
+{
+    public class UnloadAllResourceCommand : ICommand
+    {
+        public void Execute()
+        {
+            LogManager.OnDebugLog(LabelType.Event, typeof(ICommand), 
+                $"Execute as administrator <b><i>OnUnloadAllResources()</i></b>");
+            
+            ResourceManager.OnUnloadAllResources();
+            
+            LogManager.OnDebugLog(LabelType.Success, typeof(ICommand), 
+                "<b>All resources</b> are unloaded completely");
+        }
+    }
+}
