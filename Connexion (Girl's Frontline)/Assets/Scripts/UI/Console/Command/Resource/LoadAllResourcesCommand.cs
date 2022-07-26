d37c262cc8210e2bb13292bdb22f7c21a5e91f92
GloryDay <alwaysgloryday@gmail.com>
2022-07-26 23:49:10 +0900

    Upload Script LoadAllResourcesCommand.cs

---
+++ Connexion (Girl's Frontline)/Assets/Scripts/UI/Console/Command/Resource/LoadAllResourcesCommand.cs
@@ -0,0 +1,23 @@
+﻿#region NAMESPACE API
+
+using Manager;
+using LabelType = Manager.Log.Label.LabelType;
+
+#endregion
+
+namespace UI.Console.Command.Resource
+{
+    public class LoadAllResourceCommand : ICommand
+    {
+        public void Execute()
+        {
+            LogManager.OnDebugLog(LabelType.Event, typeof(ICommand), 
+                $"Execute as administrator <b><i>OnLoadAllResources()</i></b>");
+            
+            ResourceManager.OnLoadAllResources();
+            
+            LogManager.OnDebugLog(LabelType.Success, typeof(ICommand), 
+                "<b>All resources</b> are loaded completely");
+        }
+    }
+}
