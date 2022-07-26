59748d1b784cd83a3d56258d698ef91b4d5d1e75
GloryDay <alwaysgloryday@gmail.com>
2022-07-26 23:47:18 +0900

    Upload Script CommandName.cs

---
+++ Connexion (Girl's Frontline)/Assets/Scripts/UI/Console/CommandName.cs
@@ -0,0 +1,14 @@
+﻿namespace UI.Console
+{
+    public static class CommandName
+    {
+        public const string LoadAllDataCommand             = "OnLoadAllData";
+        public const string LoadAllResourcesCommand        = "OnLoadAllResources";
+        public const string UnloadAllResourcesCommand      = "OnUnloadAllResources";
+        public const string IsAllResourcesLoadedCommand    = "IsLoadedAllResourcesDone";
+        public const string InitializeBackgroundAudioMixer = "OnInitializeBackgoundAudioMixer";
+        public const string ChangeBackgroundAudioClip      = "OnChangeBackgroundAudioClip --CurrentScene";
+        public const string InstantiateAllUIPrefabsCommand = "OnInstantiateAllUIPrefabs";
+        public const string ApplicationQuit                = "OnApplicationQuit";
+    }
+}
