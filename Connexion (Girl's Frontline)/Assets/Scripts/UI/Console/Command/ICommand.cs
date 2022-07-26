4f9ab2a9f6a214b4638ce32334cbbc695d563f19
GloryDay <alwaysgloryday@gmail.com>
2022-07-26 23:49:35 +0900

    Upload Script ICommand.cs

---
+++ Connexion (Girl's Frontline)/Assets/Scripts/UI/Console/Command/ICommand.cs
@@ -0,0 +1,10 @@
+﻿namespace UI.Console.Command
+{
+    public interface ICommand
+    {
+        /// <summary>
+        /// Execute command with administrator permission
+        /// </summary>
+        void Execute();
+    }
+}
