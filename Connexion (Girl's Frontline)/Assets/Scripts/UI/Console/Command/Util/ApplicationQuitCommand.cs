9c52b5e8c54415ae3c9daa3e2983ed2f372ec15b
GloryDay <alwaysgloryday@gmail.com>
2022-07-26 23:47:36 +0900

    Upload Script ApplicationQuitCommand.cs

---
+++ Connexion (Girl's Frontline)/Assets/Scripts/UI/Console/Command/Util/ApplicationQuitCommand.cs
@@ -0,0 +1,20 @@
+﻿#region NAMESPACE API
+
+using Manager;
+using LabelType = Manager.Log.Label.LabelType;
+
+#endregion
+
+namespace UI.Console.Command.Util
+{
+    public class ApplicationQuitCommand : ICommand
+    {
+        public void Execute()
+        {
+            LogManager.OnDebugLog(LabelType.Event, typeof(ICommand), 
+                $"Execute as administrator <b><i>OnQuit()</i></b>");
+            
+            GameManager.OnQuit();
+        }
+    }
+}
