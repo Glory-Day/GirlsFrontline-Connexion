4501ed0a8e056cc74f77a6e249973bbef6d58a06
Glory-Day <alwaysgloryday@gmail.com>
2022-07-28 01:24:42 +0900

    Upload Script ApplicationSetUpCommand.cs

---
+++ Connexion (Girl's Frontline)/Assets/Scripts/UI/Console/Command/Util/ApplicationSetUpCommand.cs
@@ -0,0 +1,69 @@
+﻿#region NAMESPACE API
+
+using System.Threading.Tasks;
+using Manager;
+using LabelType = Manager.Log.Label.LabelType;
+
+using UI.Console.Command.Data;
+using UI.Console.Command.Resource;
+using UI.Console.Command.Sound;
+using UI.Console.Command.UI;
+
+#endregion
+
+namespace UI.Console.Command.Util
+{
+    public class ApplicationSetUpCommand : ICommand
+    {
+        private readonly ICommand[] loadCommands;
+        private readonly ICommand[] initializeCommands;
+
+        public ApplicationSetUpCommand()
+        {
+            loadCommands = new ICommand[]
+            {
+                new LoadAllDataCommand(), 
+                new LoadAllResourcesCommand()
+            };
+
+            initializeCommands = new ICommand[]
+            {
+                new InstantiateAllUIPrefabsCommand(),
+                new InitializeBackgroundAudioMixerCommand(),
+                new ChangeBackgroundAudioClipCommand()
+            };
+        }
+
+        private static bool IsLoadedAllResourcesDone()
+        {
+            LogManager.OnDebugLog(LabelType.Event, typeof(ICommand), 
+                $"<b>Waiting All Resources</b> is loaded");
+            
+            while (!ResourceManager.IsLoadedAllResourcesDone()) {}
+
+            return true;
+        }
+        
+        public async void Execute()
+        {
+            LogManager.OnDebugLog("Execute <b><i>OnApplicationSetUp()</i></b>");
+            
+            for (var i = 0; i < loadCommands.Length; i++)
+            {
+                loadCommands[i].Execute();
+            }
+
+            await Task.Run(() => Task.FromResult(IsLoadedAllResourcesDone()));
+            
+            LogManager.OnDebugLog(LabelType.Success, typeof(ICommand), 
+                "<b>All Resources</b> are loaded successfully");
+            
+            while (!ResourceManager.IsLoadedAllResourcesDone()) {}
+            
+            for (var i = 0; i < initializeCommands.Length; i++)
+            {
+                initializeCommands[i].Execute();
+            }
+        }
+    }
+}
