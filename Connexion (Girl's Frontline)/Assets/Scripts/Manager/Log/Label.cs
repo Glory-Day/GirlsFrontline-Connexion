281963e3261e2a834becb8dd46b602dfa269a6be
Glory-Day <alwaysgloryday@gmail.com>
2022-07-28 01:21:39 +0900

    Update Script Label.cs

--- Connexion (Girl's Frontline)/Assets/Scripts/Manager/Log/Label.cs
+++ Connexion (Girl's Frontline)/Assets/Scripts/Manager/Log/Label.cs
@@ -5,24 +5,27 @@
     /// </summary>
     public static class Label
     {
-        // Log Label Text
-        public const string DefaultLogLabel = "DEFAULT";
-        public const string EventLogLabel   = "EVENT";
-        public const string ErrorLogLabel   = "ERROR";
-        public const string WarningLogLabel = "WARNING";
-        public const string SuccessLogLabel = "SUCCESS";
-
-        // Log Label Color
-        public const string DefaultLogColor = "#C0C0C0";
-        public const string EventLogColor   = "#F8F8FF";
-        public const string ErrorLogColor   = "#DC143C";
-        public const string WarningLogColor = "#F7E600";
-        public const string SuccessLogColor = "#39FF14";
+        // Log label text for unity editor
+        public const string DefaultLogLabel                    = "DEFAULT";
+        public const string EventLogLabel                      = "EVENT";
+        public const string ErrorLogLabel                      = "ERROR";
+        public const string AdministratorInUnityEditorLogLabel = "[PERMISSON][ADMINISTRATOR]";
+        public const string SuccessLogLabel                    = "SUCCESS";
         
-        // Log Label Type
+        // Log label text for development build
+        public const string AdministratorInDevelopmentBuildLogLabel = "PERMISSION|ADMINISTRATOR";
+
+        // Log label color
+        public const string DefaultLogColor       = "#C0C0C0";
+        public const string EventLogColor         = "#F8F8FF";
+        public const string ErrorLogColor         = "#DC143C";
+        public const string AdministratorLogColor = "#F7E600";
+        public const string SuccessLogColor       = "#39FF14";
+        
+        // Log label type
         public enum LabelType
         {
-            Event = 0, Error, Warning, Success
+            Event = 0, Error, Administrator, Success
         }
     }
 }
