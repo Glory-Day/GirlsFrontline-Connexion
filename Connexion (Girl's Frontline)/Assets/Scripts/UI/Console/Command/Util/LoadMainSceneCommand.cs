26e7dd4686cfae7ccf38f4f6917041aa51abc84c
Glory-Day <alwaysgloryday@gmail.com>
2022-07-28 01:24:54 +0900

    Upload Script LoadMainSceneCommand.cs

---
+++ Connexion (Girl's Frontline)/Assets/Scripts/UI/Console/Command/Util/LoadMainSceneCommand.cs
@@ -0,0 +1,19 @@
+﻿#region NAMESPACE API
+
+using Manager;
+using SceneName = Manager.SceneManager.SceneName;
+
+#endregion
+
+namespace UI.Console.Command.Util
+{
+    public class LoadMainScene : ICommand
+    {
+        public void Execute()
+        {
+            LogManager.OnDebugLog("Execute <b><i>OnLoadSceneByName(SceneName.MainScene)</i></b>");
+            
+            SceneManager.OnLoadSceneByName(SceneName.MainScene);
+        }
+    }
+}
