9455a36e0c99122af0d3ba2b64036cdaf0fe5002
Glory-Day <alwaysgloryday@gmail.com>
2022-07-28 01:22:12 +0900

    Update Script LoadAllResourcesCommand.cs

--- Connexion (Girl's Frontline)/Assets/Scripts/UI/Console/Command/Resource/LoadAllResourcesCommand.cs
+++ Connexion (Girl's Frontline)/Assets/Scripts/UI/Console/Command/Resource/LoadAllResourcesCommand.cs
@@ -7,17 +7,16 @@ using LabelType = Manager.Log.Label.LabelType;
 
 namespace UI.Console.Command.Resource
 {
-    public class LoadAllResourceCommand : ICommand
+    public class LoadAllResourcesCommand : ICommand
     {
         public void Execute()
         {
-            LogManager.OnDebugLog(LabelType.Event, typeof(ICommand), 
-                $"Execute as administrator <b><i>OnLoadAllResources()</i></b>");
+            LogManager.OnDebugLog("Execute <b><i>OnLoadAllResources()</i></b>");
             
             ResourceManager.OnLoadAllResources();
             
             LogManager.OnDebugLog(LabelType.Success, typeof(ICommand), 
-                "<b>All Resources</b> are loaded completely");
+                "<b>All Resources</b> are loaded successfully");
         }
     }
 }
