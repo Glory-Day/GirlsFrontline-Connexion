2e4457ea5e112239a3ab5042722f9d652dd5cea5
Glory-Day <alwaysgloryday@gmail.com>
2022-07-28 01:24:25 +0900

    Upload Script ApplicationPlayCommand.cs

---
+++ Connexion (Girl's Frontline)/Assets/Scripts/UI/Console/Command/Util/ApplicationPlayCommand.cs
@@ -0,0 +1,18 @@
+﻿#region NAMESPACE API
+
+using Manager;
+
+#endregion
+
+namespace UI.Console.Command.Util
+{
+    public class ApplicationPlayCommand : ICommand
+    {
+        public void Execute()
+        {
+            LogManager.OnDebugLog("Execute <b><i>OnApplicationPlay()</i></b>");
+            
+            GameManager.OnPlay();
+        }
+    }
+}
