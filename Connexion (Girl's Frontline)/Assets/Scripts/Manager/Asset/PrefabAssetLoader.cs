c75aff103d223033b5f75ab603d5f148e8454367
GloryDay <alwaysgloryday@gmail.com>
2022-07-29 04:48:07 +0900

    Update Script PrefabAssetLoader.cs

--- Connexion (Girl's Frontline)/Assets/Scripts/Manager/Asset/PrefabAssetLoader.cs
+++ Connexion (Girl's Frontline)/Assets/Scripts/Manager/Asset/PrefabAssetLoader.cs
@@ -2,57 +2,60 @@
 
 using System;
 using System.Collections.Generic;
-
 using UnityEngine;
 using UnityEngine.AddressableAssets;
 using UnityEngine.ResourceManagement.AsyncOperations;
-
 using LabelType = Manager.Log.Label.LabelType;
 
 #endregion
 
-namespace Manager.Resource
+namespace Manager.Asset
 {
-    public static class PrefabAssetLoader
+    public class PrefabAssetLoader
     {
-        private static AsyncOperationHandle<IList<GameObject>> _uiPrefabAssetHandle;
+        private AsyncOperationHandle<IList<GameObject>> uiPrefabAssetHandle;
+
+        public PrefabAssetLoader()
+        {
+            uiPrefabAssetHandle = new AsyncOperationHandle<IList<GameObject>>();
+        }
 
         #region LOAD ASSET API
 
         /// <summary>
         /// Load UI prefab assets using <b>DataManager.AddressableLabelData</b>
         /// </summary>
-        public static void OnLoadUIPrefabAssets()
+        public void LoadUIPrefabAssets()
         {
-            LogManager.OnDebugLog(typeof(PrefabAssetLoader), 
-                $"OnLoadUIPrefabAssets()");
+            LogManager.OnDebugLog(typeof(PrefabAssetLoader),
+                $"LoadUIPrefabAssets()");
 
             void Loaded(GameObject loadedGameObject)
             {
                 UIManager.AddUIPrefabs(loadedGameObject.name, loadedGameObject);
-                
+
                 LogManager.OnDebugLog(LabelType.Success, typeof(PrefabAssetLoader),
                     $"<b>{loadedGameObject.name}</b> is loaded successfully");
             }
-            
-            _uiPrefabAssetHandle = Addressables.LoadAssetsAsync(DataManager.AddressableLabelData.prefabs[0],
+
+            uiPrefabAssetHandle = Addressables.LoadAssetsAsync(DataManager.AddressableLabelData.prefabAsset.names[0],
                 (Action<GameObject>)Loaded);
         }
 
         #endregion
 
         #region UNLOAD ASSET API
-        
+
         /// <summary>
         /// Unload UI prefab assets using <b>DataManager.AddressableLabelData</b>
         /// </summary>
-        public static void OnUnloadUIPrefabAssets()
+        public void UnloadUIPrefabAssets()
         {
-            LogManager.OnDebugLog(typeof(AudioAssetLoader), 
-                $"OnUnloadUIPrefabAssets()");
-            
-            Addressables.Release(_uiPrefabAssetHandle);
-            
+            LogManager.OnDebugLog(typeof(AudioAssetLoader),
+                $"UnloadUIPrefabAssets()");
+
+            Addressables.Release(uiPrefabAssetHandle);
+
             LogManager.OnDebugLog(LabelType.Success, typeof(AudioAssetLoader),
                 $"<b>All UI Prefabs</b> are unloaded successfully");
         }
@@ -62,7 +65,9 @@ namespace Manager.Resource
         /// <summary>
         /// Check all UI prefab assets loaded is done
         /// </summary>
-        public static bool IsLoadedUIPrefabAssetsDone() => 
-            _uiPrefabAssetHandle.IsValid() && _uiPrefabAssetHandle.IsDone;
+        public bool IsLoadedUIPrefabAssetsDone()
+        {
+            return uiPrefabAssetHandle.IsValid() && uiPrefabAssetHandle.IsDone;
+        }
     }
 }
