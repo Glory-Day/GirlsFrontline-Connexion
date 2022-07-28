c3f383e6c6308c1bdfd0ea9e6531f3775cfa7fc9
GloryDay <alwaysgloryday@gmail.com>
2022-07-29 04:51:55 +0900

    Update Script ApplicationSetUpCommand.cs

---
+++ Connexion (Girl's Frontline)/Assets/Scripts/Manager/UI/Console/Command/ApplicationSetUpCommand.cs
@@ -0,0 +1,58 @@
+﻿#region NAMESPACE API
+
+using System.Threading.Tasks;
+using Manager.UI.Console.Command;
+using LabelType = Manager.Log.Label.LabelType;
+
+#endregion
+
+namespace Manager.UI.Console.Command
+{
+    public class ApplicationSetUpCommand : ICommand
+    {
+        private readonly ICommand[] loadCommands;
+        private readonly ICommand[] initializeCommands;
+
+        public ApplicationSetUpCommand()
+        {
+            loadCommands = new ICommand[]
+                           {
+                               new LoadAllDataCommand(),
+                               new LoadAllResourcesCommand()
+                           };
+
+            initializeCommands = new ICommand[]
+                                 {
+                                     new InstantiateAllUIPrefabsCommand(),
+                                     new InitializeBackgroundAudioMixerCommand(),
+                                     new ChangeBackgroundAudioClipCommand()
+                                 };
+        }
+
+        private static bool IsLoadedAllResourcesDone()
+        {
+            LogManager.OnDebugLog(LabelType.Event, typeof(ICommand),
+                $"<b>Waiting All Resources</b> is loaded");
+
+            while(!AssetManager.IsLoadedAllAssetsDone()) { }
+
+            return true;
+        }
+
+        public async void Execute()
+        {
+            LogManager.OnDebugLog("Execute <b><i>OnApplicationSetUp()</i></b>");
+
+            for (var i = 0; i < loadCommands.Length; i++) loadCommands[i].Execute();
+
+            await Task.Run(() => Task.FromResult(IsLoadedAllResourcesDone()));
+
+            LogManager.OnDebugLog(LabelType.Success, typeof(ICommand),
+                "<b>All Resources</b> are loaded successfully");
+
+            while(!AssetManager.IsLoadedAllAssetsDone()) { }
+
+            for (var i = 0; i < initializeCommands.Length; i++) initializeCommands[i].Execute();
+        }
+    }
+}
