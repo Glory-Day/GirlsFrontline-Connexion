f375229325a94ad1f8bf8e4eff62ffd9ae7c8456
GloryDay <alwaysgloryday@gmail.com>
2022-07-29 04:58:27 +0900

    Upload Script ObjectContainer.cs

---
+++ Connexion (Girl's Frontline)/Assets/Scripts/Manager/Util/ObjectPool/ObjectContainer.cs
@@ -0,0 +1,41 @@
+﻿namespace Manager.Util.ObjectPool
+{
+    /// <summary>
+    /// Container to contain object
+    /// </summary>
+    /// <typeparam name="T"> Type of contained object </typeparam>
+    public class ObjectContainer<T>
+    {
+        public ObjectContainer(T instantiatedObject)
+        {
+            Used = true;
+            Object = instantiatedObject;
+        }
+
+        /// <summary>
+        /// Whether to use an object
+        /// </summary>
+        public bool Used { get; private set; }
+
+        /// <summary>
+        /// Contained object
+        /// </summary>
+        public T Object { get; set; }
+
+        /// <summary>
+        /// Set contained object to use
+        /// </summary>
+        public void Use()
+        {
+            Used = true;
+        }
+
+        /// <summary>
+        /// Release contained object
+        /// </summary>
+        public void Release()
+        {
+            Used = false;
+        }
+    }
+}
