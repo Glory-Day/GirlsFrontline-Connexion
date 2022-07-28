bd2392f3ff8e14217f91e7c64c7457010c105773
GloryDay <alwaysgloryday@gmail.com>
2022-07-29 04:48:28 +0900

    Update Script AddressableLabelData.cs

--- Connexion (Girl's Frontline)/Assets/Scripts/Manager/Data/Category/AddressableLabelData.cs
+++ Connexion (Girl's Frontline)/Assets/Scripts/Manager/Data/Category/AddressableLabelData.cs
@@ -6,13 +6,35 @@ using System;
 
 namespace Manager.Data.Category
 {
+    #region ASSET LABEL DATA API
+
+    /// <summary>
+    /// Audio asset label data in <b>AddressableLabel.json</b>
+    /// </summary>
+    [Serializable]
+    public class AudioAsset
+    {
+        public string[] names;
+    }
+
+    /// <summary>
+    /// Prefab asset label data in <b>AddressableLabel.json</b>
+    /// </summary>
+    [Serializable]
+    public class PrefabAsset
+    {
+        public string[] names;
+    }
+
+    #endregion
+
     /// <summary>
     /// Addressable label data in <b>AddressableLabel.json</b>
     /// </summary>
     [Serializable]
     public class AddressableLabelData
     {
-        public string[] audios;
-        public string[] prefabs;
+        public AudioAsset  audioAsset;
+        public PrefabAsset prefabAsset;
     }
 }
