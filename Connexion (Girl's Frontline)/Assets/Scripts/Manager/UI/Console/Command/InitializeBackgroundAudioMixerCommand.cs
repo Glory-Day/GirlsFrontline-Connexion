cd9ceccedb28a31c426018324518b41bb2e2ccf0
GloryDay <alwaysgloryday@gmail.com>
2022-07-29 04:52:33 +0900

    Update Script InitializeBackgroundAudioMixerCommand.cs

--- Connexion (Girl's Frontline)/Assets/Scripts/Manager/UI/Console/Command/InitializeBackgroundAudioMixerCommand.cs
+++ Connexion (Girl's Frontline)/Assets/Scripts/Manager/UI/Console/Command/InitializeBackgroundAudioMixerCommand.cs
@@ -1,21 +1,20 @@
 ﻿#region NAMESPACE API
 
-using Manager;
 using LabelType = Manager.Log.Label.LabelType;
 
 #endregion
 
-namespace UI.Console.Command.Sound
+namespace Manager.UI.Console.Command
 {
     public class InitializeBackgroundAudioMixerCommand : ICommand
     {
         public void Execute()
         {
             LogManager.OnDebugLog("Execute <b><i>OnInitializeBackgroundAudioMixer()</i></b>");
-            
+
             SoundManager.OnInitializeBackgroundAudioMixer();
-            
-            LogManager.OnDebugLog(LabelType.Success, typeof(ICommand), 
+
+            LogManager.OnDebugLog(LabelType.Success, typeof(ICommand),
                 $"<b>Background Audio Mixer</b> is initialized successfully");
         }
     }
