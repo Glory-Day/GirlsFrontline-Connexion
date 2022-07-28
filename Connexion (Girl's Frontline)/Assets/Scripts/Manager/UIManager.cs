84b26d3ae2903e856bd18dacb495a242a23a25c7
GloryDay <alwaysgloryday@gmail.com>
2022-07-29 04:57:46 +0900

    Update Script UIManager.cs

--- Connexion (Girl's Frontline)/Assets/Scripts/Manager/UIManager.cs
+++ Connexion (Girl's Frontline)/Assets/Scripts/Manager/UIManager.cs
@@ -1,9 +1,7 @@
 ﻿#region NAMESPACE API
 
 using System.Collections.Generic;
-
 using UnityEngine;
-
 using LabelType = Manager.Log.Label.LabelType;
 
 #endregion
@@ -22,7 +20,7 @@ namespace Manager
         #region ANIMATION NAME API
 
         // Screen transition animation names
-        private const string ScreenTransitionToLeftAnimation = "Screen_Transition_To_Left_Animation";
+        private const string ScreenTransitionToLeftAnimation  = "Screen_Transition_To_Left_Animation";
         private const string ScreenTransitionToRightAnimation = "Screen_Transition_To_Right_Animation";
 
         #endregion
@@ -37,46 +35,47 @@ namespace Manager
         /// </summary>
         public static void OnInstantiateAllUIPrefabs()
         {
-            LogManager.OnDebugLog(typeof(UIManager), 
+            LogManager.OnDebugLog(typeof(UIManager),
                 $"OnInstantiateAllUIPrefabs()");
-            
-            InstantiateTransitionScreenPrefab();
-            InstantiatePauseScreenPrefabs();
-            
-            LogManager.OnDebugLog(LabelType.Success, typeof(DataManager), 
+
+            Instance.InstantiateTransitionScreenPrefab();
+            Instance.InstantiatePauseScreenPrefabs();
+
+            LogManager.OnDebugLog(LabelType.Success, typeof(DataManager),
                 "<b>All UI Prefabs</b> are instantiated successfully");
         }
 
         /// <summary>
         /// Instantiate <b>Transition Screen</b>
         /// </summary>
-        private static void InstantiateTransitionScreenPrefab()
+        private void InstantiateTransitionScreenPrefab()
         {
-            LogManager.OnDebugLog(typeof(UIManager), 
+            LogManager.OnDebugLog(typeof(UIManager),
                 $"OnInstantiateTransitionScreenPrefab()");
-            
-            var gameObject = Instantiate(Instance.uiPrefabs[DataManager.ResourceData.uiPrefab.names[0]],
-                Instance.transform, true);
-            Instance.screenTransitionAnimation = gameObject.transform.GetChild(0).gameObject.GetComponent<Animation>();
-            
-            LogManager.OnDebugLog(LabelType.Success,typeof(UIManager), 
+
+            var instantiatedObject = Instantiate(uiPrefabs[DataManager.AssetData.uiPrefab.names[0]],
+                transform, true);
+            screenTransitionAnimation = instantiatedObject.transform.GetChild(0)
+                                                          .gameObject.GetComponent<Animation>();
+
+            LogManager.OnDebugLog(LabelType.Success, typeof(UIManager),
                 $"Instantiate <b>Transition Screen Prefab</b> successfully");
         }
 
         /// <summary>
         /// Instantiate <b>Pause Screen</b>
         /// </summary>
-        private static void InstantiatePauseScreenPrefabs()
+        private void InstantiatePauseScreenPrefabs()
         {
-            LogManager.OnDebugLog(typeof(UIManager), 
-                    $"InstantiatePauseScreenPrefabs()");
-            
-            var gameObject = Instantiate(Instance.uiPrefabs[DataManager.ResourceData.uiPrefab.names[1]], 
-                Instance.transform, true);
-            gameObject.SetActive(false);
-            Instance.uiPrefabs[DataManager.ResourceData.uiPrefab.names[1]] = gameObject;
+            LogManager.OnDebugLog(typeof(UIManager),
+                $"InstantiatePauseScreenPrefabs()");
 
-            LogManager.OnDebugLog(LabelType.Success,typeof(UIManager), 
+            var instantiatedObject = Instantiate(uiPrefabs[DataManager.AssetData.uiPrefab.names[1]],
+                transform, true);
+            instantiatedObject.SetActive(false);
+            uiPrefabs[DataManager.AssetData.uiPrefab.names[1]] = instantiatedObject;
+
+            LogManager.OnDebugLog(LabelType.Success, typeof(UIManager),
                 $"Instantiate <b>Pause Screen Prefab</b> successfully");
         }
 
@@ -85,28 +84,30 @@ namespace Manager
         /// </summary>
         public static void OnInitializeUIPrefabs()
         {
-            LogManager.OnDebugLog(typeof(UIManager), 
+            LogManager.OnDebugLog(typeof(UIManager),
                 $"OnInitializeUIPrefabs()");
 
             Instance.uiPrefabs = new Dictionary<string, GameObject>();
         }
-        
+
         /// <summary>
         /// Add UI prefabs in <b>List&lt;GameObject&gt;</b>
         /// </summary>
         /// <param name="key"> <b>string</b> type key value </param>
         /// <param name="gameObject"> UI Prefab </param>
-        public static void AddUIPrefabs(string key, GameObject gameObject) => 
+        public static void AddUIPrefabs(string key, GameObject gameObject)
+        {
             Instance.uiPrefabs.Add(key, gameObject);
+        }
 
         /// <summary>
         /// Set screen transition animation to <b>LeftScreenTransitionAnimation</b>
         /// </summary>
         public static void SetScreenTransitionDirectionToLeft()
         {
-            LogManager.OnDebugLog(typeof(UIManager), 
+            LogManager.OnDebugLog(typeof(UIManager),
                 $"SetScreenTransitionDirectionToLeft()");
-            
+
             Instance.screenTransitionAnimation.clip =
                 Instance.screenTransitionAnimation.GetClip(ScreenTransitionToLeftAnimation);
         }
@@ -116,9 +117,9 @@ namespace Manager
         /// </summary>
         public static void SetScreenTransitionDirectionToRight()
         {
-            LogManager.OnDebugLog(typeof(UIManager), 
+            LogManager.OnDebugLog(typeof(UIManager),
                 $"SetScreenTransitionDirectionToRight()");
-            
+
             Instance.screenTransitionAnimation.clip =
                 Instance.screenTransitionAnimation.GetClip(ScreenTransitionToRightAnimation);
         }
@@ -128,31 +129,35 @@ namespace Manager
         /// </summary>
         public static void OnPlayScreenTransitionAnimation()
         {
-            LogManager.OnDebugLog(typeof(UIManager), 
-                $"OnPlayScreenTransitionAnimation()</b>");
-            
+            LogManager.OnDebugLog(typeof(UIManager),
+                $"OnPlayScreenTransitionAnimation()");
+
             Instance.screenTransitionAnimation.Play();
-            
-            LogManager.OnDebugLog(LabelType.Event,typeof(UIManager), 
+
+            LogManager.OnDebugLog(LabelType.Event, typeof(UIManager),
                 $"Play <b>{Instance.screenTransitionAnimation.clip.name}</b>");
         }
 
         /// <summary>
         /// Enable <b>Pause Screen</b> for pause <b>Game Application</b>
         /// </summary>
-        public static void OnEnablePauseScreen() =>
-            Instance.uiPrefabs?[DataManager.ResourceData.uiPrefab.names[1]].SetActive(true);
+        public static void OnEnablePauseScreen()
+        {
+            Instance.uiPrefabs?[DataManager.AssetData.uiPrefab.names[1]].SetActive(true);
+        }
 
         /// <summary>
         /// Disable <b>Pause Screen</b>
         /// </summary>
-        public static void OnDisablePauseScreen() => 
-            Instance.uiPrefabs?[DataManager.ResourceData.uiPrefab.names[1]].SetActive(false);
+        public static void OnDisablePauseScreen()
+        {
+            Instance.uiPrefabs?[DataManager.AssetData.uiPrefab.names[1]].SetActive(false);
+        }
 
         /// <summary>
         /// Get Transform of <b>UIManager</b>
         /// </summary>
         /// <returns> Transform of <b>UIManager</b> </returns>
-        public static Transform GetTransform() => Instance.transform;
+        public static Transform Transform => Instance.transform;
     }
 }
