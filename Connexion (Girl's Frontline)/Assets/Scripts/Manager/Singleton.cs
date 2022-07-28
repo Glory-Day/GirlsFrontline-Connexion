724c7b8c2a78c032cef29e99a6f4bd71fb92143e
GloryDay <alwaysgloryday@gmail.com>
2022-07-29 04:51:03 +0900

    Update Script Singleton.cs

--- Connexion (Girl's Frontline)/Assets/Scripts/Manager/Singleton.cs
+++ Connexion (Girl's Frontline)/Assets/Scripts/Manager/Singleton.cs
@@ -1,7 +1,6 @@
 ﻿#region NAMESPACE API
 
 using System.Diagnostics.CodeAnalysis;
-
 using UnityEngine;
 using Object = UnityEngine.Object;
 
@@ -27,32 +26,26 @@ namespace Manager
                 // The singleton is gameObject. OnDestroy() doesn't use it or If uses it, let's make a null check
                 if (_quited)
                 {
-                    Debug.LogWarning("<color=#F7E600><b>[WARNING][Singleton]</b> Instance '" + typeof(T) + 
-                                     "' already destroyed. Returning null.</color>");
-                    
+                    Debug.LogWarning($"<color=#F7E600><b>[WARNING][Singleton]</b> Instance '{typeof(T)}'" +
+                                     " already destroyed. Returning null.</color>");
+
                     return null;
                 }
 
                 // Double-checked locking (Thread Safe)
                 lock (Locked)
                 {
-                    if (_instance != null)
-                    {
-                        return _instance;
-                    }
+                    if (_instance != null) return _instance;
 
                     // Search for existing instance
                     _instance = FindObjectOfType(typeof(T)) as T;
 
-                    if (_instance != null)
-                    {
-                        return _instance;
-                    }
+                    if (_instance != null) return _instance;
 
                     // If it hasn't been created yet, create an instance
                     var gameObject = new GameObject();
                     _instance = gameObject.AddComponent<T>();
-                    gameObject.name = typeof(T).Name + " (Singleton)";
+                    gameObject.name = typeof(T).Name + "(Singleton)";
 
                     // Make instance persistent
                     DontDestroyOnLoad(gameObject);
@@ -66,8 +59,8 @@ namespace Manager
         {
             _quited = true;
         }
-        
-        private void OnDestroy() 
+
+        private void OnDestroy()
         {
             _quited = true;
         }
