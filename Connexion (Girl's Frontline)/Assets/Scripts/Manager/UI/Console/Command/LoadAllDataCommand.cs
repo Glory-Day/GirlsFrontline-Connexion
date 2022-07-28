3d6bca70a80562161bb99258545071f4d5fd7856
GloryDay <alwaysgloryday@gmail.com>
2022-07-29 04:53:14 +0900

    Update Script LoadAllDataCommand.cs

--- Connexion (Girl's Frontline)/Assets/Scripts/Manager/UI/Console/Command/LoadAllDataCommand.cs
+++ Connexion (Girl's Frontline)/Assets/Scripts/Manager/UI/Console/Command/LoadAllDataCommand.cs
@@ -1,17 +1,11 @@
-﻿#region NAMESPACE API
-
-using Manager;
-
-#endregion
-
-namespace UI.Console.Command.Data
+﻿namespace Manager.UI.Console.Command
 {
     public class LoadAllDataCommand : ICommand
     {
         public void Execute()
         {
             LogManager.OnDebugLog("Execute <b><i>OnLoadAllData()</i></b>");
-            
+
             DataManager.OnLoadAllData();
         }
     }
