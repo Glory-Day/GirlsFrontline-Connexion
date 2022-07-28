9434436cf6495f2df049d8163077520af906aab8
GloryDay <alwaysgloryday@gmail.com>
2022-07-29 04:53:03 +0900

    Update Script IsLoadedAllResourcesDoneCommand.cs

--- Connexion (Girl's Frontline)/Assets/Scripts/Manager/UI/Console/Command/IsLoadedAllResourcesDoneCommand.cs
+++ Connexion (Girl's Frontline)/Assets/Scripts/Manager/UI/Console/Command/IsLoadedAllResourcesDoneCommand.cs
@@ -1,27 +1,26 @@
 ﻿#region NAMESPACE API
 
-using Manager;
 using LabelType = Manager.Log.Label.LabelType;
 
 #endregion
 
-namespace UI.Console.Command.Resource
+namespace Manager.UI.Console.Command
 {
     public class IsLoadedAllResourcesDoneCommand : ICommand
     {
         public void Execute()
         {
             LogManager.OnDebugLog("Execute <b><i>IsLoadedAllResourcesDone()</i></b>");
-            
-            if (ResourceManager.IsLoadedAllResourcesDone())
+
+            if (AssetManager.IsLoadedAllAssetsDone())
             {
-                LogManager.OnDebugLog(LabelType.Success, typeof(ICommand), 
+                LogManager.OnDebugLog(LabelType.Success, typeof(ICommand),
                     $"<b>All Resources</b> is loaded successfully");
 
                 return;
             }
-            
-            LogManager.OnDebugLog(LabelType.Error, typeof(ICommand), 
+
+            LogManager.OnDebugLog(LabelType.Error, typeof(ICommand),
                 $"<b>All Resources</b> is not loaded");
         }
     }
