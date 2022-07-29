9b473b4d53d62356f2859ad4adca5476b23533b0
Glory-Day <alwaysgloryday@gmail.com>
2022-07-29 22:20:54 +0900

    Upload Script LogBuilder.cs

--- Connexion (Girl's Frontline)/Assets/Scripts/Manager/Log/UnityEditor/LogBuilder.cs
+++ Connexion (Girl's Frontline)/Assets/Scripts/Manager/Log/UnityEditor/LogBuilder.cs
@@ -9,14 +9,14 @@ namespace Manager.Log.UnityEditor
     /// <summary>
     /// Output log string builder by purpose in <b>Unity Editor Console</b>
     /// </summary>
-    public static class LogBuilder
+    public class LogBuilder
     {
         /// <summary>
         /// Build the string of the administrator permission log
         /// </summary>
         /// <param name="contents"> Contents of the log </param>
         /// <returns> The string of the administrator permission log </returns>
-        public static string OnBuild(string contents)
+        public string Build(string contents)
         {
             return $"<color={Label.AdministratorLogColor}><b>{Label.AdministratorInUnityEditorLogLabel}</b> " +
                    $"{contents.Replace('_', ' ')}</color>";
@@ -28,7 +28,7 @@ namespace Manager.Log.UnityEditor
         /// <param name="classType"> The type of the class where the log was called </param>
         /// <param name="contents"> Contents of the log </param>
         /// <returns> The string of the default log </returns>
-        public static string OnBuild(Type classType, string contents)
+        public string Build(Type classType, string contents)
         {
             return $"<color={Label.DefaultLogColor}><b>[{classType.Name}]</b>" +
                    $" Called <b><i>{contents.Replace('_', ' ')}</i></b></color>";
@@ -42,7 +42,7 @@ namespace Manager.Log.UnityEditor
         /// <param name="contents"> Contents of the log </param>
         /// <returns> The string of special log </returns>
         /// <exception cref="ArgumentOutOfRangeException"> Out of range exception in <b>LabelType</b> </exception>
-        public static string OnBuild(Label.LabelType type, Type classType, string contents)
+        public string Build(Label.LabelType type, Type classType, string contents)
         {
             string log;
 
