78c51c28714a393afa892012f5905b02a2ba0106
GloryDay <alwaysgloryday@gmail.com>
2022-07-31 17:35:23 +0900

    Update Script StateMachine.cs

---
+++ Connexion (Girl's Frontline)/Assets/Scripts/Object/Util/State/StateMachine.cs
@@ -0,0 +1,46 @@
+﻿namespace Object.Util.State
+{
+    public class StateMachine
+    {
+        /// <summary>
+        /// Current state of object
+        /// </summary>
+        public IState CurrentState { get; private set; }
+
+        public StateMachine(IState state)
+        {
+            CurrentState = state;
+            CurrentState.Enter();
+        }
+        
+        /// <summary>
+        /// Exit current state and set new state
+        /// </summary>
+        /// <param name="state"> New state </param>
+        public void SetState(IState state)
+        {
+            // Do not set same state
+            if (CurrentState == state)
+            {
+                return;
+            }
+            
+            // Before state change, exit state
+            CurrentState.Exit();
+
+            // Change state
+            CurrentState = state;
+            
+            // Enter new state
+            CurrentState.Enter();
+        }
+        
+        /// <summary>
+        /// Update is called once per frame
+        /// </summary>
+        public void OnUpdate()
+        {
+            CurrentState.Update();
+        }
+    }
+}
