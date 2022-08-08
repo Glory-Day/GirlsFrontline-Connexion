7baffdf7a0286507b7cdde26fa5cba367a422376
Glory-Day <alwaysgloryday@gmail.com>
2022-08-08 21:06:38 +0900

    Update Script LogBuilder.cs

--- Connexion (Girl's Frontline)/Assets/Scripts/Manager/Log/DevelopmentBuild/LogBuilder.cs
+++ Connexion (Girl's Frontline)/Assets/Scripts/Manager/Log/DevelopmentBuild/LogBuilder.cs
@@ -9,16 +9,16 @@ namespace Manager.Log.DevelopmentBuild
     /// <summary>
     /// Output log string builder by purpose in <b>Game Application</b> after build
     /// </summary>
-    public class LogBuilder
+    public static class LogBuilder
     {
         /// <summary>
         /// Build the string of the administrator permission log
         /// </summary>
         /// <param name="contents"> Contents of the log </param>
         /// <returns> The string of the administrator permission log </returns>
-        public string Build(string contents)
+        public static string Build(string contents)
         {
-            return $"{Label.AdministratorInDevelopmentBuildLogLabel}|{contents.Replace('_', ' ')}";
+            return $"{Label.AdministratorInDevelopmentBuildLogLabel}|{contents}";
         }
 
         /// <summary>
@@ -27,10 +27,9 @@ namespace Manager.Log.DevelopmentBuild
         /// <param name="classType"> The type of the class where the log was called </param>
         /// <param name="contents"> Contents of the log </param>
         /// <returns> The string of the default log </returns>
-        public string Build(Type classType, string contents)
+        public static string Build(Type classType, string contents)
         {
-            return $"{Label.DefaultLogLabel}|{classType.Name}|" +
-                   $"Called <b><i>{contents.Replace('_', ' ')}</i></b>";
+            return $"{Label.DefaultLogLabel}|{classType.Name}|Called <b ><i >{contents}</i ></b >";
         }
 
         /// <summary>
@@ -41,20 +40,20 @@ namespace Manager.Log.DevelopmentBuild
         /// <param name="contents"> Contents of the log </param>
         /// <returns> The string of special log </returns>
         /// <exception cref="ArgumentOutOfRangeException"> Out of range in <b>LabelType</b> </exception>
-        public string Build(Label.LabelType type, Type classType, string contents)
+        public static string Build(Label.LabelType type, Type classType, string contents)
         {
             string log;
 
             switch (type)
             {
                 case Label.LabelType.Event:
-                    log = $"{Label.EventLogLabel}|{classType.Name}|{contents.Replace('_', ' ')}";
+                    log = $"{Label.EventLogLabel}|{classType.Name}|{contents}";
                     break;
                 case Label.LabelType.Error:
-                    log = $"{Label.ErrorLogLabel}|{classType.Name}|{contents.Replace('_', ' ')}";
+                    log = $"{Label.ErrorLogLabel}|{classType.Name}|{contents}";
                     break;
                 case Label.LabelType.Success:
-                    log = $"{Label.SuccessLogLabel}|{classType.Name}|{contents.Replace('_', ' ')}";
+                    log = $"{Label.SuccessLogLabel}|{classType.Name}|{contents}";
                     break;
                 default:
                     throw new ArgumentOutOfRangeException(nameof(type), type, null);
