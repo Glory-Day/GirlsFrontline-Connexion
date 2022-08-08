0937043bce546e8d362849adf6a975c34a8c485f
Glory-Day <alwaysgloryday@gmail.com>
2022-08-08 21:06:56 +0900

    Update Script LogBuilder.cs

--- Connexion (Girl's Frontline)/Assets/Scripts/Manager/Log/UnityEditor/LogBuilder.cs
+++ Connexion (Girl's Frontline)/Assets/Scripts/Manager/Log/UnityEditor/LogBuilder.cs
@@ -9,17 +9,17 @@ namespace Manager.Log.UnityEditor
     /// <summary>
     /// Output log string builder by purpose in <b>Unity Editor Console</b>
     /// </summary>
-    public class LogBuilder
+    public static class LogBuilder
     {
         /// <summary>
         /// Build the string of the administrator permission log
         /// </summary>
         /// <param name="contents"> Contents of the log </param>
         /// <returns> The string of the administrator permission log </returns>
-        public string Build(string contents)
+        public static string Build(string contents)
         {
-            return $"<color={Label.AdministratorLogColor}><b>{Label.AdministratorInUnityEditorLogLabel}</b> " +
-                   $"{contents.Replace('_', ' ')}</color>";
+            return $"<color={Label.AdministratorLogColor}><b>{Label.AdministratorInUnityEditorLogLabel}</b>" +
+                   $"{contents}</color>";
         }
 
         /// <summary>
@@ -28,10 +28,9 @@ namespace Manager.Log.UnityEditor
         /// <param name="classType"> The type of the class where the log was called </param>
         /// <param name="contents"> Contents of the log </param>
         /// <returns> The string of the default log </returns>
-        public string Build(Type classType, string contents)
+        public static string Build(Type classType, string contents)
         {
-            return $"<color={Label.DefaultLogColor}><b>[{classType.Name}]</b>" +
-                   $" Called <b><i>{contents.Replace('_', ' ')}</i></b></color>";
+            return $"<color={Label.DefaultLogColor}><b>[{classType.Name}]</b>Called <b><i>{contents}</i></b></color>";
         }
 
         /// <summary>
@@ -42,7 +41,7 @@ namespace Manager.Log.UnityEditor
         /// <param name="contents"> Contents of the log </param>
         /// <returns> The string of special log </returns>
         /// <exception cref="ArgumentOutOfRangeException"> Out of range exception in <b>LabelType</b> </exception>
-        public string Build(Label.LabelType type, Type classType, string contents)
+        public static string Build(Label.LabelType type, Type classType, string contents)
         {
             string log;
 
@@ -50,15 +49,15 @@ namespace Manager.Log.UnityEditor
             {
                 case Label.LabelType.Event:
                     log = $"<color={Label.EventLogColor}><b>{Label.EventLogLabel}[{classType.Name}]</b> " +
-                          $"{contents.Replace('_', ' ')}</color>";
+                          $"{contents}</color>";
                     break;
                 case Label.LabelType.Error:
                     log = $"<color={Label.ErrorLogColor}><b>{Label.ErrorLogLabel}[{classType.Name}]</b> " +
-                          $"{contents.Replace('_', ' ')}</color>";
+                          $"{contents}</color>";
                     break;
                 case Label.LabelType.Success:
                     log = $"<color={Label.SuccessLogColor}><b>{Label.SuccessLogLabel}[{classType.Name}]</b> " +
-                          $"{contents.Replace('_', ' ')}</color>";
+                          $"{contents}</color>";
                     break;
                 default:
                     throw new ArgumentOutOfRangeException(nameof(type), type, null);
