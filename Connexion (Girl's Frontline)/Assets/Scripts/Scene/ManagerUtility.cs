1b253439f0de6910d5976858caf9a8d9195374eb
GloryDay <alwaysgloryday@gmail.com>
2022-08-11 22:42:18 +0900

    Update Script ManagerUtility.cs

---
+++ Connexion (Girl's Frontline)/Assets/Scripts/Scene/ManagerUtility.cs
@@ -0,0 +1,21 @@
+﻿#region NAMESPACE API
+
+using UnityEngine;
+using Manager;
+
+#endregion
+
+namespace Scene
+{
+    public class ManagerUtility : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        private void Start()
+        {
+            AssetManager.OnInitialize();
+            SoundManager.OnInitialize();
+            ObjectManager.OnInitialize();
+            UIManager.OnInitialize();
+        }
+    }
+}
