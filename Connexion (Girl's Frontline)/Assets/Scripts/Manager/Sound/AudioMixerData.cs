050a20c48f6d6ab52f1d8a00a5935cc000d2f2f6
GloryDay <alwaysgloryday@gmail.com>
2022-08-17 23:22:24 +0900

    Upload Script AudioMixerData.cs

---
+++ Connexion (Girl's Frontline)/Assets/Scripts/Manager/Sound/AudioMixerData.cs
@@ -0,0 +1,28 @@
+﻿#region NAMESPACE API
+
+using UnityEngine;
+using UnityEngine.Audio;
+
+#endregion
+
+namespace Manager.Sound
+{
+    public class AudioMixerData : MonoBehaviour
+    {
+        [Header("Master Audio Mixer")]
+        [SerializeField]
+        public AudioMixer masterAudioMixer;
+        
+        [Header("Background Audio Mixer Group")]
+        [SerializeField]
+        public AudioMixerGroup backgroundAudioMixerGroup;
+        
+        [Header("Effect Audio Mixer Group")]
+        [SerializeField]
+        public AudioMixerGroup effectAudioMixerGroup;
+        
+        [Header("Voice Audio Mixer Group")]
+        [SerializeField]
+        public AudioMixerGroup voiceAudioMixerGroup;
+    }
+}
