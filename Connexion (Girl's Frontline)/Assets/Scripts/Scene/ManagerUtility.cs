9b210fb14e2aef42aeb82d21cb3f76fe6be03fba
GloryDay <alwaysgloryday@gmail.com>
2022-08-17 23:23:35 +0900

    Update Script ManagerUtility.cs

--- Connexion (Girl's Frontline)/Assets/Scripts/Scene/ManagerUtility.cs
+++ Connexion (Girl's Frontline)/Assets/Scripts/Scene/ManagerUtility.cs
@@ -7,11 +7,17 @@ using Manager;
 
 namespace Scene
 {
+    /// <summary>
+    /// 
+    /// </summary>
     public class ManagerUtility : MonoBehaviour
     {
         // Start is called before the first frame update
         private void Start()
         {
+            LogManager.OnDebugLog(typeof(ManagerUtility),
+                "Start()");
+            
             AssetManager.OnInitialize();
             SoundManager.OnInitialize();
             ObjectManager.OnInitialize();
