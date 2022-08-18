161e8b52963cff2c87e66ed450d22d0b7f612334
GloryDay <alwaysgloryday@gmail.com>
2022-08-18 21:45:37 +0900

    Update Script AudioMixerGroupData.cs

---
+++ Connexion (Girl's Frontline)/Assets/Scripts/Manager/Sound/AudioMixerGroupData.cs
@@ -0,0 +1,37 @@
+﻿#region NAMESPACE API
+
+using System;
+using UnityEngine;
+using UnityEngine.Audio;
+
+#endregion
+
+namespace Manager.Sound
+{
+    public class AudioMixerGroupData : MonoBehaviour
+    {
+        [Header("Master Audio Mixer Group")]
+        [SerializeField]
+        private AudioMixerGroup master;
+        
+        [Header("Background Audio Mixer Group")]
+        [SerializeField]
+        private AudioMixerGroup background;
+        
+        [Header("Effect Audio Mixer Group")]
+        [SerializeField]
+        private AudioMixerGroup effect;
+        
+        [Header("Voice Audio Mixer Group")]
+        [SerializeField]
+        private AudioMixerGroup voice;
+
+        public AudioMixerGroup Master => master;
+
+        public AudioMixerGroup Background => background;
+
+        public AudioMixerGroup Effect => effect;
+
+        public AudioMixerGroup Voice => voice;
+    }
+}
