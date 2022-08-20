309a2f677882e5fa3225948d5903674b02011bde
GloryDay <alwaysgloryday@gmail.com>
2022-08-20 17:29:22 +0900

    Upload Script SkipButtonEvent.cs

---
+++ Connexion (Girl's Frontline)/Assets/Scripts/Event/SkipButtonEvent.cs
@@ -0,0 +1,32 @@
+﻿#region NAMESPACE API
+
+using UnityEngine;
+using Manager;
+using UI = UnityEngine.UI;
+using LabelType = Manager.Log.Label.LabelType;
+using SceneName = Manager.SceneManager.SceneName;
+
+#endregion
+
+namespace Event.Button
+{
+    public class SkipButtonEvent : MonoBehaviour
+    {
+        #region BUTTON EVENT API
+
+        /// <summary>
+        /// <see cref="UI.Button"/> event to skip video when clicked button in <b>Introduction Video Scene</b>
+        /// </summary>
+        public void OnClicked()
+        {
+            LogManager.OnDebugLog(
+                LabelType.Event,
+                typeof(SkipButtonEvent),
+                "<b>Skip Button</b> is clicked");
+            
+            SceneManager.OnLoadSceneByName(SceneName.MainScene);
+        }
+
+        #endregion
+    }
+}
