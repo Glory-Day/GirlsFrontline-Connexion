f3237958cc34ffd62b8dba6ee254ba5061c8d742
GloryDay <alwaysgloryday@gmail.com>
2022-08-23 22:46:38 +0900

    Update Script InstantiateAllUIPrefabsCommand.cs

---
+++ Connexion (Girl's Frontline)/Assets/Scripts/Object/Console/Command/InstantiateAllUIPrefabsCommand.cs
@@ -0,0 +1,20 @@
+﻿#region NAMESPACE API
+
+using Interface;
+using Manager;
+
+#endregion
+
+namespace Object.Console.Command
+{
+    public class InstantiateAllUIPrefabsCommand : ICommand
+    {
+        public void Execute()
+        {
+            LogManager.OnDebugLog(
+                "Execute <b><i>OnInstantiateAllUIPrefabs()</i></b>");
+
+            UIManager.OnInstantiateAllUIPrefabs();
+        }
+    }
+}
