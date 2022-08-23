f6a242f492d90e69214feaa9cb1a5ff1114ef419
GloryDay <alwaysgloryday@gmail.com>
2022-08-23 22:45:32 +0900

    Update Script ApplicationPlayCommand.cs

---
+++ Connexion (Girl's Frontline)/Assets/Scripts/Object/Console/Command/ApplicationPlayCommand.cs
@@ -0,0 +1,20 @@
+﻿#region NAMESPACE API
+
+using Interface;
+using Manager;
+
+#endregion
+
+namespace Object.Console.Command
+{
+    public class ApplicationPlayCommand : ICommand
+    {
+        public void Execute()
+        {
+            LogManager.OnDebugLog(
+                "Execute <b><i>OnApplicationPlay()</i></b>");
+
+            GameManager.OnPlay();
+        }
+    }
+}
