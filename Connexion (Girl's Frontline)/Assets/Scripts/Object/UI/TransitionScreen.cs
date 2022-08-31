2db61b56e989d887bc5542401a391b45c989334c
GloryDay <alwaysgloryday@gmail.com>
2022-08-31 22:28:45 +0900

    Update Script TransitionScreen.cs

---
+++ Connexion (Girl's Frontline)/Assets/Scripts/Object/UI/TransitionScreen.cs
@@ -0,0 +1,56 @@
+﻿#region NAMESPACE API
+
+using System;
+using UnityEngine;
+using Manager;
+using Label = Manager.Log.Label;
+
+#endregion
+
+namespace Object.UI
+{
+    public class TransitionScreen : MonoBehaviour
+    {
+        #region ANIMATION EVENT API
+
+        public void OnLoadSceneWhenSceneTransitionToLeft()
+        {
+            LogManager.OnDebugLog(
+                Label.Event, 
+                typeof(TransitionScreen),
+                $"<b>Scene Transition Animation Event</b> is activated. Transition direction is <b>Left</b>");
+
+            switch (SceneManager.CurrentSceneName)
+            {
+                case Manager.Scene.Label.Main:
+                    SceneManager.OnLoadSceneByLabel(Manager.Scene.Label.Selection);
+                    break;
+                case Manager.Scene.Label.Selection:
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+
+        public void OnLoadSceneWhenSceneTransitionToRight()
+        {
+            LogManager.OnDebugLog(
+                Label.Event, 
+                typeof(TransitionScreen),
+                $"<b>Scene Transition Animation Event</b> is activated. Transition direction is <b>Right</b>");
+
+            switch (SceneManager.CurrentSceneName)
+            {
+                case Manager.Scene.Label.Main:
+                    break;
+                case Manager.Scene.Label.Selection:
+                    SceneManager.OnLoadSceneByLabel(Manager.Scene.Label.Main);
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+
+        #endregion
+    }
+}
