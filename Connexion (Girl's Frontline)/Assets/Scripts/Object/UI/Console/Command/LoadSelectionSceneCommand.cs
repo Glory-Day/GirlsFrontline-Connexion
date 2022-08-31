bb3be590901f6011bb7c5f7058ffbdd9e8b41afd
GloryDay <alwaysgloryday@gmail.com>
2022-08-31 15:49:08 +0900

    Update Script LoadSelectionSceneCommand.cs

--- Connexion (Girl's Frontline)/Assets/Scripts/Object/UI/Console/Command/LoadSelectionSceneCommand.cs
+++ Connexion (Girl's Frontline)/Assets/Scripts/Object/UI/Console/Command/LoadSelectionSceneCommand.cs
@@ -1,18 +1,18 @@
 ﻿#region NAMESPACE API
 
-using Interface;
 using Manager;
 using SceneName = Manager.SceneManager.SceneName;
 
 #endregion
 
-namespace Object.Console.Command
+namespace Object.UI.Console.Command
 {
     public class LoadSelectionScene : ICommand
     {
         public void Execute()
         {
-            LogManager.OnDebugLog("Execute <b><i>OnLoadSceneByName(SceneName.SelectionScene)</i></b>");
+            LogManager.OnDebugLog(
+                "Execute <b><i>OnLoadSceneByName(SceneName.SelectionScene)</i></b>");
 
             SceneManager.OnLoadSceneByName(SceneName.SelectionScene);
         }
