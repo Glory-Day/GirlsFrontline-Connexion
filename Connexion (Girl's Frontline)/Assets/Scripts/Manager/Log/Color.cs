69b7a4705fa318745ba17bb3b59198e582a0a8f6
GloryDay <alwaysgloryday@gmail.com>
2022-08-31 22:25:04 +0900

    Upload Script Color.cs

---
+++ Connexion (Girl's Frontline)/Assets/Scripts/Manager/Log/Color.cs
@@ -0,0 +1,11 @@
+﻿namespace Manager.Log
+{
+    public static class Color
+    {
+        public const string Default       = "#C0C0C0";
+        public const string Event         = "#F8F8FF";
+        public const string Error         = "#DC143C";
+        public const string Administrator = "#F7E600";
+        public const string Success       = "#39FF14";
+    }
+}
