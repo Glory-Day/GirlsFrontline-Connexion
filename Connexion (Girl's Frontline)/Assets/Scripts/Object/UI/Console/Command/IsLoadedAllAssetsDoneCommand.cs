45aeda985c9f3b04bd01ae8c5da09be2c0037e01
GloryDay <alwaysgloryday@gmail.com>
2022-08-31 15:48:19 +0900

    Update Script IsLoadedAllAssetDoneCommand.cs

--- Connexion (Girl's Frontline)/Assets/Scripts/Object/UI/Console/Command/IsLoadedAllAssetsDoneCommand.cs
+++ Connexion (Girl's Frontline)/Assets/Scripts/Object/UI/Console/Command/IsLoadedAllAssetsDoneCommand.cs
@@ -1,28 +1,32 @@
 ﻿#region NAMESPACE API
 
-using Interface;
 using Manager;
 using LabelType = Manager.Log.Label.LabelType;
 
 #endregion
 
-namespace Object.Console.Command
+namespace Object.UI.Console.Command
 {
     public class IsLoadedAllAssetsDoneCommand : ICommand
     {
         public void Execute()
         {
-            LogManager.OnDebugLog("Execute <b><i>IsLoadedAllAssetsDoneCommand()</i></b>");
+            LogManager.OnDebugLog(
+                "Execute <b><i>IsLoadedAllAssetsDoneCommand()</i></b>");
 
             if (AssetManager.IsLoadedAllAssetsDone())
             {
-                LogManager.OnDebugLog(LabelType.Success, typeof(ICommand),
+                LogManager.OnDebugLog(
+                    LabelType.Success, 
+                    typeof(ICommand),
                     $"<b>All Resources</b> is loaded successfully");
 
                 return;
             }
 
-            LogManager.OnDebugLog(LabelType.Error, typeof(ICommand),
+            LogManager.OnDebugLog(
+                LabelType.Error, 
+                typeof(ICommand),
                 $"<b>All Resources</b> is not loaded");
         }
     }
