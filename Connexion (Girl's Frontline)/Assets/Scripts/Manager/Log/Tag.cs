49bdd63922acf0aca09a59261b1fcfdaeabce301
GloryDay <alwaysgloryday@gmail.com>
2022-08-31 22:25:42 +0900

    Upload Script Tag.cs

--- Connexion (Girl's Frontline)/Assets/Scripts/Manager/Log/Tag.cs
+++ Connexion (Girl's Frontline)/Assets/Scripts/Manager/Log/Tag.cs
@@ -13,23 +13,4 @@
             public const string DevelopmentBuild = "PERMISSION|ADMINISTRATOR";
         }
     }
-
-    public static class Color
-    {
-        public const string Default       = "#C0C0C0";
-        public const string Event         = "#F8F8FF";
-        public const string Error         = "#DC143C";
-        public const string Administrator = "#F7E600";
-        public const string Success       = "#39FF14";
-    }
-
-    public static class LogLabel
-    {
-        public enum Label
-        {
-            Event,
-            Error,
-            Success
-        }
-    }
 }
