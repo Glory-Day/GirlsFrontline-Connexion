6afee09c88abb287f8780a22f71e19a7c5e9ba6a
GloryDay <alwaysgloryday@gmail.com>
2022-08-31 15:48:58 +0900

    Update Script LoadMainSceneCommand.cs

--- Connexion (Girl's Frontline)/Assets/Scripts/Object/UI/Console/Command/LoadMainSceneCommand.cs
+++ Connexion (Girl's Frontline)/Assets/Scripts/Object/UI/Console/Command/LoadMainSceneCommand.cs
@@ -1,18 +1,18 @@
 ﻿#region NAMESPACE API
 
-using Interface;
 using Manager;
 using SceneName = Manager.SceneManager.SceneName;
 
 #endregion
 
-namespace Object.Console.Command
+namespace Object.UI.Console.Command
 {
     public class LoadMainScene : ICommand
     {
         public void Execute()
         {
-            LogManager.OnDebugLog("Execute <b><i>OnLoadSceneByName(SceneName.MainScene)</i></b>");
+            LogManager.OnDebugLog(
+                "Execute <b><i>OnLoadSceneByName(SceneName.MainScene)</i></b>");
 
             SceneManager.OnLoadSceneByName(SceneName.MainScene);
         }
