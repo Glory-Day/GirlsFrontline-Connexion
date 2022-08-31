a9c4f40d00ea8f06b059cf7ec5cb1c36972a816b
GloryDay <alwaysgloryday@gmail.com>
2022-08-31 15:49:18 +0900

    Update Script UnloadAllAssetsCommand.cs

--- Connexion (Girl's Frontline)/Assets/Scripts/Object/UI/Console/Command/UnloadAllAssetsCommand.cs
+++ Connexion (Girl's Frontline)/Assets/Scripts/Object/UI/Console/Command/UnloadAllAssetsCommand.cs
@@ -1,22 +1,24 @@
 ﻿#region NAMESPACE API
 
-using Interface;
 using Manager;
 using LabelType = Manager.Log.Label.LabelType;
 
 #endregion
 
-namespace Object.Console.Command
+namespace Object.UI.Console.Command
 {
     public class UnloadAllAssetsCommand : ICommand
     {
         public void Execute()
         {
-            LogManager.OnDebugLog("Execute <b><i>OnUnloadAllAssets()</i></b>");
+            LogManager.OnDebugLog(
+                "Execute <b><i>OnUnloadAllAssets()</i></b>");
 
             AssetManager.OnUnloadAllAssets();
 
-            LogManager.OnDebugLog(LabelType.Success, typeof(ICommand),
+            LogManager.OnDebugLog(
+                LabelType.Success, 
+                typeof(ICommand),
                 "<b>All Assets</b> are unloaded successfully");
         }
     }
