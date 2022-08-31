043e22ba9e8c8bfd43ce8f914a03ec100afade9a
GloryDay <alwaysgloryday@gmail.com>
2022-08-31 15:43:08 +0900

    Update Script GameData.cs

--- Connexion (Girl's Frontline)/Assets/Scripts/Manager/Data/Category/GameData.cs
+++ Connexion (Girl's Frontline)/Assets/Scripts/Manager/Data/Category/GameData.cs
@@ -8,9 +8,6 @@ namespace Manager.Data.Category
 {
     #region DATA API
 
-    /// <summary>
-    /// Data format of chapter lock in <see cref="GameData"/>
-    /// </summary>
     [Serializable]
     public class Chapter
     {
@@ -19,9 +16,6 @@ namespace Manager.Data.Category
 
     #endregion
 
-    /// <summary>
-    /// Data format in <see cref="GameData"/> Json file
-    /// </summary>
     [Serializable]
     public class GameData
     {
