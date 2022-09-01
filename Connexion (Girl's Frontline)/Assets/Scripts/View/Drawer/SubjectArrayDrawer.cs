0d0d611f7ad87b21aa675142c235da1c7e3fa761
Glory-Day <alwaysgloryday@gmail.com>
2022-09-01 22:01:41 +0900

    Update Script SubjectArrayDrawer.cs

---
+++ Connexion (Girl's Frontline)/Assets/Scripts/View/Drawer/SubjectArrayDrawer.cs
@@ -0,0 +1,36 @@
+﻿#region NAMESPACE API
+
+using UnityEngine;
+using UnityEditor;
+
+#endregion
+
+namespace View.Drawer
+{
+    [CustomPropertyDrawer(typeof(SubjectArrayAttribute))]
+    public class SubjectArrayDrawer : PropertyDrawer
+    {
+        public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+        {
+            return EditorGUI.GetPropertyHeight(property, label, property.isExpanded);
+        }
+
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            try
+            {
+                var index = int.Parse(property.propertyPath.Split('[', ']')[1]);
+                EditorGUI.PropertyField(
+                    position, property, new GUIContent($"{Subject} {Start + index}"), property.isExpanded);
+            }
+            catch
+            {
+                EditorGUI.PropertyField(position, property, label, property.isExpanded);
+            }
+        }
+        
+        private string Subject => ((SubjectArrayAttribute)attribute).Subject;
+
+        private int Start => ((SubjectArrayAttribute)attribute).Start;
+    }
+}
