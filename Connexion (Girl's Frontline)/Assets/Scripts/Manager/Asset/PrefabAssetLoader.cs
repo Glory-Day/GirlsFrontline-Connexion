5a58a6d6a7fa838cdba2edbd11d298b1bbfbdaaf
Glory-Day <alwaysgloryday@gmail.com>
2022-09-01 22:05:20 +0900

    Update Script PrefabAssetLoader.cs

--- Connexion (Girl's Frontline)/Assets/Scripts/Manager/Asset/PrefabAssetLoader.cs
+++ Connexion (Girl's Frontline)/Assets/Scripts/Manager/Asset/PrefabAssetLoader.cs
@@ -24,7 +24,7 @@ namespace Manager.Asset
             uiPrefabAssetsHandle = new AsyncOperationHandle<IList<GameObject>>();
         }
 
-        #region LOAD ASSET API
+        #region LOAD METHOD API
         
         public void LoadUIPrefabAssets()
         {
@@ -48,7 +48,7 @@ namespace Manager.Asset
 
         #endregion
 
-        #region UNLOAD ASSET API
+        #region UNLOAD METHOD API
         
         public void UnloadUIPrefabAssets()
         {
@@ -66,7 +66,7 @@ namespace Manager.Asset
 
         #endregion
 
-        #region CHECK ASSET API
+        #region CHECK METHOD API
 
         public bool IsLoadedUIPrefabAssetsDone()
         {
