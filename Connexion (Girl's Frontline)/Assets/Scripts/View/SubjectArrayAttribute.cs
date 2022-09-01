f8eb5268f8a7a4976a9ddd5aa780f3f7d5b627a6
Glory-Day <alwaysgloryday@gmail.com>
2022-09-01 22:02:05 +0900

    Update Script SubjectArrayAttribute.cs

---
+++ Connexion (Girl's Frontline)/Assets/Scripts/View/SubjectArrayAttribute.cs
@@ -0,0 +1,27 @@
+﻿#region NAMESPACE API
+
+using UnityEngine;
+
+#endregion
+
+namespace View
+{
+    public class SubjectArrayAttribute : PropertyAttribute
+    {
+        public SubjectArrayAttribute(string subject)
+        {
+            Subject = subject;
+            Start = 0;
+        }
+        
+        public SubjectArrayAttribute(string subject, int start)
+        {
+            Subject = subject;
+            Start = start;
+        }
+        
+        public string Subject { get; }
+        
+        public int Start { get; }
+    }
+}
