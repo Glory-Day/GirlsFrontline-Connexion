d12cd25c5db63347a4b1fa486daed25a7a8e205a
GloryDay <alwaysgloryday@gmail.com>
2022-09-02 23:05:07 +0900

    Upload Script NextButton.cs

---
+++ Connexion (Girl's Frontline)/Assets/Scripts/Object/UI/Chapter/Component/NextButton.cs
@@ -0,0 +1,56 @@
+﻿#region NAMESPACE API
+
+using UnityEngine;
+using Manager;
+using Label = Manager.Log.Label;
+
+#endregion
+
+namespace Object.UI.Chapter.Component
+{
+    public class NextButton : MonoBehaviour
+    {
+        #region CONSTANT FIELD
+
+        private readonly string[] animationNames =
+        {
+            "Next Button Animation 01", "Next Button Animation 02",
+            "Next Button Animation 03", "Next Button Animation 04"
+        };
+
+        #endregion
+
+        private ChapterController controller;
+        
+        // Start is called before the first frame update
+        private void Start()
+        {
+            LogManager.OnDebugLog(
+                typeof(NextButton),
+                "Start()");
+            
+            controller = GetComponentInParent<ChapterController>();
+        }
+
+        #region BUTTON EVENT API
+
+        public void OnClicked()
+        {
+            LogManager.OnDebugLog(
+                Label.Event, 
+                typeof(NextButton),
+                $"<b>Next Button</b> is clicked");
+
+            controller.SelectionAnimation.clip = controller.SelectionAnimation.GetClip(
+                animationNames[controller.CurrentChapterIndex++]);
+            controller.SelectionAnimation.Play();
+            
+            LogManager.OnDebugLog(
+                Label.Success, 
+                typeof(NextButton),
+                $"<b>Chapter 0{controller.CurrentChapterIndex + 1}</b> is selected successfully");
+        }
+
+        #endregion
+    }
+}
