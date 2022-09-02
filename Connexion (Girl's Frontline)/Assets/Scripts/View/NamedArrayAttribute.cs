3c8122b69ac369e7665f7d11e6ac021a922d7a8f
GloryDay <alwaysgloryday@gmail.com>
2022-09-02 23:03:47 +0900

    Update Script NamedArrayAttribute.cs

---
+++ Connexion (Girl's Frontline)/Assets/Scripts/View/NamedArrayAttribute.cs
@@ -0,0 +1,27 @@
+﻿#region NAMESPACE API
+
+using UnityEngine;
+
+#endregion
+
+namespace View
+{
+    public class NamedArrayAttribute : PropertyAttribute
+    {
+        public NamedArrayAttribute(string name)
+        {
+            Name = name;
+            Start = 0;
+        }
+        
+        public NamedArrayAttribute(string name, int start)
+        {
+            Name = name;
+            Start = start;
+        }
+        
+        public string Name { get; }
+        
+        public int Start { get; }
+    }
+}
