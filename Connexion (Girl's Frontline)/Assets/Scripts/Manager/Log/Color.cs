9cb751eba1dad296af49b19e588aa0254e592be4
GloryDay <alwaysgloryday@gmail.com>
2022-09-06 22:46:46 +0900

    Update Script Color.cs

--- Connexion (Girl's Frontline)/Assets/Scripts/Manager/Log/Color.cs
+++ Connexion (Girl's Frontline)/Assets/Scripts/Manager/Log/Color.cs
@@ -2,10 +2,14 @@
 {
     public static class Color
     {
+        #region CONSTANT FIELD
+
         public const string Default       = "#C0C0C0";
         public const string Event         = "#F8F8FF";
         public const string Error         = "#DC143C";
         public const string Administrator = "#F7E600";
         public const string Success       = "#39FF14";
+
+        #endregion
     }
 }
