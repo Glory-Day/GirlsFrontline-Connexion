333a0236407b5bda416d63ca02a305615573a6ae
GloryDay <alwaysgloryday@gmail.com>
2022-09-06 22:46:40 +0900

    Update Script JsonFilePath.cs

--- Connexion (Girl's Frontline)/Assets/Scripts/Manager/Data/JsonFilePath.cs
+++ Connexion (Girl's Frontline)/Assets/Scripts/Manager/Data/JsonFilePath.cs
@@ -2,9 +2,13 @@
 {
     public static class JsonFilePath
     {
+        #region CONSTANT FIELD
+
         public const string AddressableLabelDataPath = "/AddressableLabelData.json";
         public const string GameDataPath             = "/GameData.json";
         public const string SceneDataPath            = "/SceneData.json";
         public const string AssetDataPath            = "/AssetData.json";
+
+        #endregion
     }
 }
