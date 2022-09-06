73ef56920ffff982a4948d33d0c0c3722b66b5f6
GloryDay <alwaysgloryday@gmail.com>
2022-09-06 22:47:44 +0900

    Update Script NextButton.cs

---
+++ Connexion (Girl's Frontline)/Assets/Scripts/Object/UI/Selection/Component/NextButton.cs
@@ -0,0 +1,71 @@
+﻿#region NAMESPACE API
+
+using System;
+using UnityEngine;
+using Manager;
+using Label = Manager.Log.Label;
+
+#endregion
+
+namespace Object.UI.Selection.Component
+{
+    public class NextButton : MonoBehaviour
+    {
+        #region CONSTANT FIELD
+
+        private readonly string[] animationNames =
+        {
+            "Next Button Animation 01", "Next Button Animation 02",
+            "Next Button Animation 03", "Next Button Animation 04"
+        };
+
+        #endregion
+
+        #region CALLBACK API
+
+        private event Action IncreaseChapterIndexCallBack;
+        private event Func<int> GetCurrentChapterIndexCallBack;
+        
+        #endregion
+
+        private Animation selectionAnimation;
+
+        // Start is called before the first frame update
+        private void Start()
+        {
+            LogManager.OnDebugLog(
+                typeof(NextButton),
+                "Start()");
+            
+            var component = GetComponentInParent<SelectionController>();
+            selectionAnimation = component.SelectionAnimation;
+            IncreaseChapterIndexCallBack = component.IncreaseChapterIndex;
+            GetCurrentChapterIndexCallBack = component.GetCurrentChapterIndex;
+        }
+
+        #region BUTTON EVENT API
+
+        public void OnClicked()
+        {
+            LogManager.OnDebugLog(
+                Label.Event, 
+                typeof(NextButton),
+                $"<b>Next Button</b> is clicked");
+
+            if (GetCurrentChapterIndexCallBack == null) return;
+
+            selectionAnimation.clip = 
+                selectionAnimation.GetClip(animationNames[GetCurrentChapterIndexCallBack.Invoke()]);
+            selectionAnimation.Play();
+
+            IncreaseChapterIndexCallBack?.Invoke();
+
+            LogManager.OnDebugLog(
+                Label.Success,
+                typeof(NextButton),
+                $"<b>Chapter 0{GetCurrentChapterIndexCallBack.Invoke() + 1}</b> is selected successfully");
+        }
+
+        #endregion
+    }
+}
