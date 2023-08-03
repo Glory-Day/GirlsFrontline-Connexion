aad5f06a4cd66ff4261aed4c6d3cbaf0e0aa777f
Glory Day <alwaysgloryday@gmail.com>
2023-08-03 16:44:14 +0900

    Update Script AudioAssetLoader.cs

--- GirlsFrontline-Connexion/Assets/Scripts/Util/Asset/AudioAssetLoader.cs
+++ GirlsFrontline-Connexion/Assets/Scripts/Util/Asset/AudioAssetLoader.cs
@@ -1,15 +1,11 @@
-﻿#region NAMESPACE API
-
-using System;
+﻿using System;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
 using UnityEngine.ResourceManagement.AsyncOperations;
 using Object.Manager;
 using Util.Manager;
-using Util.Manager.Log;
-
-#endregion
+using Util.Log;
 
 namespace Util.Asset
 {
