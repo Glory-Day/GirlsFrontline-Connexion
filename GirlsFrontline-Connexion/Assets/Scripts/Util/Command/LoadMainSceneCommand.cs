1786540e0e88d3a85173b2edfb35417f2ee34e48
Glory Day <alwaysgloryday@gmail.com>
2023-08-05 00:48:43 +0900

    Update Script LoadMainSceneCommand.cs

--- GirlsFrontline-Connexion/Assets/Scripts/Util/Command/LoadMainSceneCommand.cs
+++ GirlsFrontline-Connexion/Assets/Scripts/Util/Command/LoadMainSceneCommand.cs
@@ -11,7 +11,7 @@ namespace Util.Command
     {
         public void Execute()
         {
-            LogManager.OnDebugLog(
+            LogManager.LogAdministrator(
                 "Execute <b><i>OnLoadSceneByName(Scene.Label.Main)</i></b>");
 
             SceneManager.OnLoadSceneByLabel(Manager.Scene.Label.Main);
