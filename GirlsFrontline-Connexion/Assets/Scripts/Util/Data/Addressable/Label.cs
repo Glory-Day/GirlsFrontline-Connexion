4da9e012aaef6cc6e2db284a514b62532752d8da
Glory Day <alwaysgloryday@gmail.com>
2023-08-10 17:30:25 +0900

    Upload Script Label.cs

---
+++ GirlsFrontline-Connexion/Assets/Scripts/Util/Data/Addressable/Label.cs
@@ -0,0 +1,17 @@
+﻿namespace Util.Data.Addressable
+{
+    public struct Label
+    {
+        public struct Audio
+        {
+            public const string Background = "background";
+            public const string Effect     = "effect";
+            public const string Voice      = "voice";
+        }
+        
+        public struct Text
+        {
+            public const string Json = "json";
+        }
+    }
+}
