a3302b545f9505b4b44cf529a1ea878fc9cc7942
Glory Day <alwaysgloryday@gmail.com>
2023-08-16 15:22:49 +0900

    Upload Script AddressablesLabel.cs

---
+++ GirlsFrontline-Connexion/Assets/Scripts/Util/Manager/Asset/Loader/AddressablesLabel.cs
@@ -0,0 +1,22 @@
+﻿namespace Util.Manager.Asset.Loader
+{
+    public struct AddressablesLabel
+    {
+        public struct AudioClip
+        {
+            public const string Background = "audio/background";
+            public const string Effect     = "audio/effect";
+            public const string Voice      = "audio/voice";
+        }
+
+        public struct Prefab
+        {
+            public const string UI = "prefab/ui";
+        }
+
+        public struct Text
+        {
+            public const string Json = "json";
+        }
+    }
+}
