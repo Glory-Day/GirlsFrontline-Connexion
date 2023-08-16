3bc36dbec76858f73cf44f88064a19bbd1127713
Glory Day <alwaysgloryday@gmail.com>
2023-08-16 15:23:10 +0900

    Upload Script ILoader.cs

---
+++ GirlsFrontline-Connexion/Assets/Scripts/Util/Manager/Asset/Loader/ILoader.cs
@@ -0,0 +1,21 @@
+﻿namespace Util.Manager.Asset.Loader
+{
+    public interface ILoader
+    {
+        /// <summary>
+        /// Load assets using addressables
+        /// </summary>
+        void Load();
+        
+        /// <summary>
+        /// Unload assets using addressables
+        /// </summary>
+        void Unload();
+        
+        /// <summary>
+        /// Check assets is Loaded
+        /// </summary>
+        /// <returns></returns>
+        bool Check();
+    }
+}
