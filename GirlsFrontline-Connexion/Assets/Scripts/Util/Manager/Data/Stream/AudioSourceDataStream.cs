27bdb51a011d85ad2685ddf2cc3201b1bf5f093a
Glory Day <alwaysgloryday@gmail.com>
2023-08-18 00:24:10 +0900

    Upload Script AudioSourceDataStream.cs

---
+++ GirlsFrontline-Connexion/Assets/Scripts/Util/Manager/Data/Stream/AudioSourceDataStream.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+using Util.Manager.Data.Json;
+
+namespace Util.Manager.Data.Stream
+{
+    public class AudioSourceDataStream : IDataStream<AudioSourceData>
+    {
+        public AudioSourceData Load()
+        {
+            LogManager.LogProgress();
+            
+            var json = AssetManager.TextAsset.Data[nameof(AudioSourceData)].text;
+            var data = JsonConvert.DeserializeObject<AudioSourceData>(json);
+
+            return data;
+        }
+    }
+}
