a09b3432706370311f0ddc6c171659fb3c1de180
Glory Day <alwaysgloryday@gmail.com>
2023-08-18 00:22:27 +0900

    Update Script TextAssetLoader.cs

--- GirlsFrontline-Connexion/Assets/Scripts/Util/Manager/Asset/Loader/TextAssetLoader.cs
+++ GirlsFrontline-Connexion/Assets/Scripts/Util/Manager/Asset/Loader/TextAssetLoader.cs
@@ -1,25 +1,76 @@
-﻿namespace Util.Manager.Asset.Loader
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
+
+namespace Util.Manager.Asset.Loader
 {
     public class TextAssetLoader : IAssetLoader
     {
+        private struct AsyncOperationHandler
+        {
+            public AsyncOperationHandle<IList<UnityEngine.TextAsset>> data;
+        }
+
+        private AsyncOperationHandler asyncOperationHandler;
+
+        public TextAssetLoader()
+        {
+            LogManager.LogProgress();
+            
+            asyncOperationHandler = new AsyncOperationHandler();
+        }
+        
+        /// <summary>
+        /// Load data assets using addressables
+        /// </summary>
+        /// <param name="asset"> Loaded asset </param>
+        private static void LoadData(UnityEngine.TextAsset asset)
+        {
+            LogManager.LogProgress();
+            
+            AssetManager.TextAsset.Data.Add(asset.name.GetName(), asset);
+            
+            LogManager.LogSuccess($"<b>{asset.name}</b> is loaded");
+        }
+        
+        /// <summary>
+        /// Unload data assets using addressables
+        /// </summary>
+        private void UnloadData()
+        {
+            LogManager.LogProgress();
+            
+            Addressables.Release(asyncOperationHandler.data);
+
+            LogManager.LogSuccess("<b>All Data</b> are unloaded");
+        }
+        
         public void Load()
         {
+            LogManager.LogProgress();
             
+            asyncOperationHandler.data = Addressables.LoadAssetsAsync(AddressablesLabel.Text.Data,
+                (Action<UnityEngine.TextAsset>)LoadData);
         }
 
         public void Unload()
         {
+            LogManager.LogProgress();
             
+            UnloadData();
         }
 
         public bool Check()
         {
-            return false;
+            return IsDataLoadedDone;
         }
-
-        private void LoadJsonAssets()
-        {
-            
-        }
+        
+        /// <summary>
+        /// Check data assets is loaded
+        /// </summary>
+        private bool IsDataLoadedDone => 
+            asyncOperationHandler.data.IsValid() && asyncOperationHandler.data.IsDone;
     }
 }
