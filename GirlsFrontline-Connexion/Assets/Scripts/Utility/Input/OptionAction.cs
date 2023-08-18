ff2f4ec681c03142d1e6cabcd81c10c9af6d39ca
Glory Day <alwaysgloryday@gmail.com>
2023-08-18 17:18:36 +0900

    Update Script OptionAction.cs

--- GirlsFrontline-Connexion/Assets/Scripts/Utility/Input/OptionAction.cs
+++ GirlsFrontline-Connexion/Assets/Scripts/Utility/Input/OptionAction.cs
@@ -6,7 +6,7 @@ using System.Collections.Generic;
 using UnityEngine.InputSystem;
 using UnityEngine.InputSystem.Utilities;
 
-namespace Util.Input
+namespace Utility.Input
 {
     public class @OptionAction : IInputActionCollection, IDisposable
     {
