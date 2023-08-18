c5cb7519f05c62364b6e5be801e48f18f716ad0c
Glory Day <alwaysgloryday@gmail.com>
2023-08-18 17:00:45 +0900

    Upload Script TextReference.cs

---
+++ GirlsFrontline-Connexion/Assets/Scripts/Utility/Manager/Asset/Reference/TextReference.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Utility.Manager.Asset.Reference
+{
+    public class TextReference
+    {
+        public Dictionary<string, TextAsset> Data { get; } 
+            = new Dictionary<string, TextAsset>();
+    }
+}
