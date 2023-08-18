6432158d2b0e6203a3451523a7dd97a37960ed26
Glory Day <alwaysgloryday@gmail.com>
2023-08-18 17:00:16 +0900

    Upload Script AudioClipReference.cs

---
+++ GirlsFrontline-Connexion/Assets/Scripts/Utility/Manager/Asset/Reference/AudioClipReference.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Utility.Manager.Asset.Reference
+{
+    public class AudioClipReference
+    {
+        public Dictionary<string, AudioClip> Background { get; } 
+            = new Dictionary<string, AudioClip>();
+        public Dictionary<string, AudioClip> Effect { get; } 
+            = new Dictionary<string, AudioClip>();
+        public Dictionary<string, AudioClip> Voice { get; } 
+            = new Dictionary<string, AudioClip>();
+    }
+}
