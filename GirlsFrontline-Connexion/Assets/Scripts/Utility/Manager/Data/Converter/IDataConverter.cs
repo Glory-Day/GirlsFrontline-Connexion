92d619eef07a22e963062be90688f27e158a9f24
Glory Day <alwaysgloryday@gmail.com>
2023-08-18 21:16:43 +0900

    Upload Script IDataConverter.cs

---
+++ GirlsFrontline-Connexion/Assets/Scripts/Utility/Manager/Data/Converter/IDataConverter.cs
@@ -0,0 +1,10 @@
+﻿namespace Utility.Manager.Data.Converter
+{
+    public interface IDataConverter<out T>
+    {
+        /// <summary>
+        /// Convert json format data to object type format
+        /// </summary>
+        T ToObject();
+    }
+}
