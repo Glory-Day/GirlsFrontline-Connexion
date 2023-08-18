0a2e0b799c37b5e0bfa3dd46e83afd3e3e17d990
Glory Day <alwaysgloryday@gmail.com>
2023-08-18 17:00:32 +0900

    Upload Script PrefabReference.cs

---
+++ GirlsFrontline-Connexion/Assets/Scripts/Utility/Manager/Asset/Reference/PrefabReference.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Utility.Manager.Asset.Reference
+{
+    public class PrefabReference
+    {
+        public Dictionary<string, GameObject> UI { get; } 
+            = new Dictionary<string, GameObject>();
+    }
+}
