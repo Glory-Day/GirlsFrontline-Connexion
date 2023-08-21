6bd3efccadc6c9a05719e7f2639401eabd931748
Glory Day <alwaysgloryday@gmail.com>
2023-08-21 16:39:42 +0900

    Update Script SceneManager.cs

--- GirlsFrontline-Connexion/Assets/Scripts/Utility/Manager/SceneManager.cs
+++ GirlsFrontline-Connexion/Assets/Scripts/Utility/Manager/SceneManager.cs
@@ -1,42 +1,96 @@
 ﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using JetBrains.Annotations;
 using Object.Manager;
-using Utility.Manager.Scene;
+using SceneManagement = UnityEngine.SceneManagement;
 
 namespace Utility.Manager
 {
+    [PublicAPI]
     public class SceneManager : Singleton<SceneManager>
     {
-        #region STATIC METHOD API
-
-        public static void OnLoadSceneByLabel(Label label)
+        public List<string> sceneNames;
+        
+        private SceneManager()
         {
             LogManager.LogProgress();
+            
+            sceneNames = new List<string>();
+            
+            SetSceneNames();
+        }
 
-            switch (label)
+        private void SetSceneNames()
+        {
+            LogManager.LogProgress();
+            
+            for (var i = 0; i < SceneManagement.SceneManager.sceneCountInBuildSettings; i++)
             {
-                case Scene.Label.Main:
-                    UnityEngine.SceneManagement.SceneManager.LoadScene("Main Scene");
-                    SoundManager.OnChangeBackgroundAudioClip(Scene.Label.Main);
-
-                    LogManager.LogSuccess("<b>Main Scene</b> is loaded");
-                    break;
-                case Scene.Label.Selection:
-                    UnityEngine.SceneManagement.SceneManager.LoadScene("Selection Scene");
-                    SoundManager.OnChangeBackgroundAudioClip(Scene.Label.Selection);
-
-                    LogManager.LogSuccess("<b>Selection Scene</b> is loaded");
-                    break;
-                default:
-                    throw new ArgumentOutOfRangeException(nameof(label), label, null);
+                var scenePath = SceneManagement.SceneUtility.GetScenePathByBuildIndex(i);
+                sceneNames.Add(Path.GetFileNameWithoutExtension(scenePath));
             }
         }
 
-        #endregion
+        private void LoadNextScene()
+        {
+            LogManager.LogProgress();
 
-        #region STATIC PROPERTIES API
+            try
+            {
+                var sceneIndex = CurrentSceneIndex + 1;
+                var sceneName  = sceneNames[sceneIndex];
+                SceneManagement.SceneManager.LoadScene(sceneName);
+                SoundManager.OnChangeBackgroundAudioClip(sceneIndex);
+            
+                LogManager.LogSuccess($"<b>{sceneName}</b> is loaded");
+            }
+            catch (IndexOutOfRangeException exception)
+            {
+                LogManager.LogError(exception.Message);
+            }
+        }
 
-        public static Label CurrentSceneLabel =>
-            (Label)UnityEngine.SceneManagement.SceneManager.GetActiveScene().buildIndex;
+        private void LoadPreviewScene()
+        {
+            try
+            {
+                var sceneIndex = CurrentSceneIndex - 1;
+                var sceneName  = sceneNames[sceneIndex];
+                SceneManagement.SceneManager.LoadScene(sceneName);
+                SoundManager.OnChangeBackgroundAudioClip(sceneIndex);
+            
+                LogManager.LogSuccess($"<b>{sceneName}</b> is loaded");
+            }
+            catch (IndexOutOfRangeException exception)
+            {
+                LogManager.LogError(exception.Message);
+            }
+        }
+        
+        private int CurrentSceneIndex => SceneManagement.SceneManager.GetActiveScene().buildIndex;
+        
+        #region STATIC METHOD API
+
+        /// <summary>
+        /// Load the current scene to next scene
+        /// </summary>
+        public static void OnLoadNextScene()
+        {
+            LogManager.LogProgress();
+            
+            Instance.LoadNextScene();
+        }
+
+        /// <summary>
+        /// Load the current scene to preview scene
+        /// </summary>
+        public static void OnLoadPreviewScene()
+        {
+            LogManager.LogProgress();
+            
+            Instance.LoadPreviewScene();
+        }
 
         #endregion
     }
