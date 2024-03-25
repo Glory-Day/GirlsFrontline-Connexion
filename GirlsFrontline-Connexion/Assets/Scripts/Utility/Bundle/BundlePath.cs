6cfd03911d9499177dc8fc1d65575d270ba67988
Glory-Day <alwaysgloryday@gmail.com>
2024-03-25 17:32:08 +0900

    Feature: Bundle system
    Add the feature to create and import bundles

---
+++ GirlsFrontline-Connexion/Assets/Scripts/Utility/Bundle/BundlePath.cs
@@ -0,0 +1,14 @@
+﻿#if UNITY_EDITOR
+
+using UnityEngine;
+
+namespace Utility.Bundle
+{
+    public static class BundlePath
+    {
+        public static readonly string CommandConsoleBundleDirectory = Application.dataPath + "/Library/Bundles";
+        public static readonly string CommandConsoleBundlePath = CommandConsoleBundleDirectory + "/command_console";
+    }
+}
+
+#endif
---
+++ GirlsFrontline-Connexion/Assets/Scripts/Utility/Bundle/Editor/AssetBundleBuilderMenu.cs
@@ -0,0 +1,25 @@
+﻿#if UNITY_EDITOR
+
+using System.IO;
+using UnityEditor;
+
+namespace Utility.Bundle.Editor
+{
+    public static class AssetBundleBuilderMenu
+    {
+        [MenuItem("Build/Build Library Asset Bundles/Build Command Console Asset")]
+        public static void BuildCommandConsoleBundles()
+        {
+            if (Directory.Exists(BundlePath.CommandConsoleBundleDirectory) == false)
+            {
+                Directory.CreateDirectory(BundlePath.CommandConsoleBundleDirectory);
+            }
+
+            BuildPipeline.BuildAssetBundles(BundlePath.CommandConsoleBundleDirectory, 
+                                            BuildAssetBundleOptions.None,
+                                            EditorUserBuildSettings.activeBuildTarget);
+        }
+    }
+}
+
+#endif
